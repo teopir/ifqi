061744abc0d7c223cd57cb77df39b66ee18b71be
SamuelePolimi <samuele.tosatto@gmail.com>
2016-05-11 21:18:17 +0200

    models with weight decay

--- ifqi/models/mlp.py
+++ ifqi/models/mlp.py
@@ -8,24 +8,25 @@ class MLP():
                  hidden_neurons=15,
                  h_layer=1,
                  act_function="relu",
-                 optimizer=None):
+                 optimizer=None,regularizer=None):
         self.hidden_neurons = hidden_neurons
         self.optimizer = optimizer
         self.n_input = n_input
         self.n_output = n_output
         self.h_layer = h_layer
         self.act_function=act_function
+        self.regularizer = regularizer
 
     def getModel(self):
         model = Sequential()
         model.add(Dense(self.hidden_neurons,
                         input_shape=(self.n_input,),
-                        activation=self.act_function))
+                        activation=self.act_function,W_regularizer=self.regularizer,b_regularizer=self.regularizer))
         for i in range(1, self.h_layer):
             model.add(Dense(self.hidden_neurons,
-                            activation=self.act_function))
+                            activation=self.act_function,W_regularizer=self.regularizer,b_regularizer=self.regularizer))
         model.add(Dense(self.n_output,
-                        activation='linear'))
+                        activation='linear',W_regularizer=self.regularizer,b_regularizer=self.regularizer))
 
         model.compile(loss='mse', optimizer=self.optimizer)
 
