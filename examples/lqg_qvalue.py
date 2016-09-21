Fixed minor errors in LQG - added function for the computation of the J in lqg

examples/lqg_qvalue.py
@@ -11,6 +11,27 @@ from ifqi.envs.lqg1d import LQG1D
 mdp = LQG1D()
 K = mdp.computeOptimalK()
 print('Optimal K: {}'.format(K))
+
+
+class tmp_policy():
+    def __init__(self, M):
+        self.K = M
+
+    def predict(self, state):
+        return np.dot(self.K, state), 0
+
+
+##############################################################
+# Compute the discounted reward
+n_rep = 1000
+J = mdp.computeJ(K, 1e-10, n_random_x0=n_rep)
+pol = tmp_policy(K)
+Jsample = 0
+for i in range(n_rep):
+    Jsample += mdp.runEpisode(pol, False, True)[0]
+Jsample /= n_rep
+print(J, Jsample)
+
 xs = np.linspace(-mdp.max_pos, mdp.max_pos, 60)
 us = np.linspace(-mdp.max_action, mdp.max_action, 50)
 
@@ -27,7 +48,5 @@ print('printed Q-table')
 # print(tabular_Q)
 fig = plt.figure()
 ax = fig.gca(projection='3d')
-ax.scatter(tabular_Q[:,0], tabular_Q[:,1], tabular_Q[:,2])
+ax.scatter(tabular_Q[:, 0], tabular_Q[:, 1], tabular_Q[:, 2])
 plt.show()
-
-

ifqi/envs/lqg1d.py
@@ -50,6 +50,10 @@ class LQG1D(gym.Env, Environment):
         self.R = np.array([0.9]).reshape((1, 1))
         self.viewer = None
 
+        # End episode
+        self._absorbing = False
+        self._atGoal = False
+
         high = np.array([self.max_pos])
         self.action_space = spaces.Box(low=-self.max_action,
                                        high=self.max_action,
@@ -78,7 +82,7 @@ class LQG1D(gym.Env, Environment):
         return -cost
 
     def _reset(self, state=None):
-        self.state = np.array([self.np_random.uniform(low=-10, high=-10)])
+        self.state = np.array([self.np_random.uniform(low=-self.max_pos, high=self.max_pos)])
         return np.array(self.state)
 
     def _getState(self):
@@ -185,6 +189,35 @@ class LQG1D(gym.Env, Environment):
                                  np.dot(self.B.T, np.dot(P, self.A)))
         return K
 
+    def computeJ(self, K, Sigma, n_random_x0=100):
+        """
+        This function computes the discounted reward associated to the provided
+        linear controller (u = Kx + \epsilon, \epsilon \sim N(0,\Sigma)).
+        Args:
+            K (matrix): the controller matrix
+            Sigma (matrix): covariance matrix of the zero-mean noise added to the controller action
+            n_random_x0: the number of samples to draw in order to average over the initial state
+
+        Returns:
+            J (float): The discounted reward
+
+        """
+        if isinstance(K, (int, long, float, complex)):
+            K = np.array([K]).reshape(1, 1)
+        if isinstance(Sigma, (int, long, float, complex)):
+            Sigma = np.array([Sigma]).reshape(1, 1)
+
+        P = self._computeP2(K)
+        J = 0.0
+        for i in range(n_random_x0):
+            self._reset()
+            x0 = self._getState()
+            J -= np.dot(x0.T, np.dot(P, x0)) \
+                 + (1 / (1 - self.gamma)) * \
+                   np.trace(np.dot(Sigma, (self.R + self.gamma * np.dot(self.B.T, np.dot(P, self.B)))))
+        J /= n_random_x0
+        return J
+
     def computeQFunction(self, x, u, K, Sigma, n_random_xn=100):
         """
         This function computes the Q-value of a pair (x,u) given the linear
@@ -204,8 +237,10 @@ class LQG1D(gym.Env, Environment):
             x = np.array([x])
         if isinstance(u, (int, long, float, complex)):
             u = np.array([u])
+        if isinstance(K, (int, long, float, complex)):
+            K = np.array([K]).reshape(1, 1)
         if isinstance(Sigma, (int, long, float, complex)):
-            Sigma = np.array([Sigma]).reshape(1,1)
+            Sigma = np.array([Sigma]).reshape(1, 1)
 
         P = self._computeP2(K)
         Qfun = 0
@@ -282,4 +317,3 @@ class LQG1D(gym.Env, Environment):
         #
         #     P = self.to_mat(vecP)
         #     return P
-