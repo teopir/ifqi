d9dd7397b0bfc97f65d3085a31f0b0a784c9e9d7
SamuelePolimi <samuele.tosatto@gmail.com>
2016-09-30 17:56:58 +0200

    experiment 'environment' building

--- examples/experimentThread.py
+++ examples/experimentThread.py
@@ -6,37 +6,28 @@ Created on Wed Sep 28 22:08:52 2016
 """
 
 from __future__ import print_function
+
 import os
 import sys
 import cPickle
-#import numpy as np
-
-
-sys.path.insert(0, os.path.abspath('../'))
-
-#from ifqi.fqi.FQI import FQI
-#import ifqi.evaluation 
+from ifqi.utils.datasetGenerator import DatasetGenerator
+from examples.variableLoadSave import ExperimentVariables
+import ifqi.evaluation.evaluation as evaluate
+from ifqi.experiment import Experiment
 import argparse
-
-
 
 """
 
 Single thread of experimentThreadManager
 """
 
-
-from ifqi.experiment import Experiment
-#from ifqi.fqi.FQI import FQI
-from ifqi.utils.datasetGenerator import DatasetGenerator
-#from ifqi.examples.variableLoadSave import ExperimentVariables
-
-#------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
 # Retrive params
-#------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
 
 
-parser = argparse.ArgumentParser(description='Execution of one experiment thread provided a configuration file and\n\t A regressor (index)\n\t Size of dataset (index)\n\t Dataset (index)')
+parser = argparse.ArgumentParser(
+    description='Execution of one experiment thread provided a configuration file and\n\t A regressor (index)\n\t Size of dataset (index)\n\t Dataset (index)')
 
 parser.add_argument("configFile", type=str, help="Provide the filename of the configuration file")
 parser.add_argument("regressor", type=int, help="Provide the index of the regressor listed in the configuration file")
@@ -45,13 +36,12 @@ parser.add_argument("dataset", type=int, help="Provide the index of the dataset"
 args = parser.parse_args()
 
 config_file = args.configFile
-#Every experiment just run a regressor that is selected by the ExperimentThreadManager, and here we get the index
+# Every experiment just run a regressor that is selected by the ExperimentThreadManager, and here we get the index
 regressorN = args.regressor
-#Every experiment just run a specific dataset size. ExperimentThreadManager select one index of size
+# Every experiment just run a specific dataset size. ExperimentThreadManager select one index of size
 sizeN = args.size
-#Every experiment just run a specific dataset. ExperimentThreadManager select one specific dataset
+# Every experiment just run a specific dataset. ExperimentThreadManager select one specific dataset
 datasetN = args.dataset
-
 
 print("Started experiment with dataset " + str(datasetN) + ", size " + str(sizeN))
 
@@ -59,18 +49,16 @@ mainFolder = "results/"
 
 exp = Experiment(config_file)
 
-
-
-#------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
 # Variables
-#------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
 
 dir_ = mainFolder + exp.config["experimentSetting"]["savePath"]
 iterations = exp.config['rlAlgorithm']['nIteration']
 repetitions = exp.config['experimentSetting']["nRepetition"]
 
-nEvaluation = exp.config['experimentSetting']["evaluation"]['nEvaluation'] 
-evaluationEveryNIteration = exp.config['experimentSetting']["evaluation"]['everyNIteration'] 
+nEvaluation = exp.config['experimentSetting']["evaluation"]['nEvaluation']
+evaluationEveryNIteration = exp.config['experimentSetting']["evaluation"]['everyNIteration']
 
 saveFQI = False
 if "saveEveryNIteration" in exp.config['experimentSetting']:
@@ -80,41 +68,42 @@ if "saveEveryNIteration" in exp.config['experimentSetting']:
 experienceReplay = False
 if "experienceReplay" in exp.config['experimentSetting']:
     experienceReplay = True
-    nExperience = exp.config['experimentSetting']["evaluation"]['nExperience'] 
-    experienceEveryNIteration = exp.config['experimentSetting']["evaluation"]['everyNIteration'] 
+    nExperience = exp.config['experimentSetting']["evaluation"]['nExperience']
+    experienceEveryNIteration = exp.config['experimentSetting']["evaluation"]['everyNIteration']
 
-#Here I take the right size
+# Here I take the right size
 size = exp.config['experimentSetting']['datasetSize'][sizeN]
 
 environment = exp.getMDP()
 regressorName = exp.getModelName(regressorN)
 fit_params = exp.getFitParams(regressorN)
 
-ds_filename = mainFolder +  ".regressor_" + str(regressorName) + "size_" + str(size) + "dataset_" + str(datasetN) + ".npy"
-pkl_filename = mainFolder +  ".regressor_" + str(regressorName) + "size_" + str(size) + "dataset_" + str(datasetN) + ".pkl"
-#TODO:
+ds_filename = mainFolder + ".regressor_" + str(regressorName) + "size_" + str(size) + "dataset_" + str(
+    datasetN) + ".npy"
+pkl_filename = mainFolder + ".regressor_" + str(regressorName) + "size_" + str(size) + "dataset_" + str(
+    datasetN) + ".pkl"
+# TODO:
 action_dim = 1
 
-#------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
 # Dataset Generation
-#------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
 
 dataset = DatasetGenerator(environment)
 if not os.path.isfile(ds_filename):
     dataset.load(ds_filename)
-dataset.generate(nEpisodes=size)
+dataset.generate(n_episodes=nEvaluation)
 
-#TODO: i have to implement
-sast , r = dataset.sastr
+sast, r = dataset.sastr
 
-#------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
 # FQI Loading
-#------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
 
 actualRepetition = 0
 actualIteration = 0
 
-if not os.path.isfile(pkl_filename):    #if no fqi present in the directory
+if not os.path.isfile(pkl_filename):  # if no fqi present in the directory
     fqi = exp.getFQI(regressorN)
     fqi.partial_fit(sast[:], r[:], **fit_params)
     min_t = 1
@@ -125,37 +114,47 @@ else:
     fqi = fqi_obj["fqi"]
     actualIteration = fqi_obj["actualIteration"] + 1
     actualRepetition = fqi_obj["actualRepetition"]
-    
-#------------------------------------------------------------------------------
-# FQI Iterations
-#------------------------------------------------------------------------------
 
-dataset = None #
+# ------------------------------------------------------------------------------
+# FQI Iterations
+# ------------------------------------------------------------------------------
+
+varSetting = ExperimentVariables("experimentName")
 replay_experience = False
-for repetition in xrange(actualRepetition, repetitions):
-    for iteration in xrange(actualIteration, iterations):
-        
-        #----------------------------------------------------------------------
+for repetition in range(actualRepetition, repetitions):
+    for iteration in range(actualIteration, iterations):
+
+        # ----------------------------------------------------------------------
         # Fit
-        #----------------------------------------------------------------------
-        
+        # ----------------------------------------------------------------------
+
         if replay_experience:
             fqi.partial_fit(sast[:], r[:], **fit_params)
-            replay_experience=False
+            replay_experience = False
         else:
             fqi.partial_fit(None, None, **fit_params)
-                
-        
-        #----------------------------------------------------------------------
+
+        # ----------------------------------------------------------------------
+        # Evaluation
+        # ----------------------------------------------------------------------
+
+        if iteration % evaluationEveryNIteration == 0:
+            score, stdScore, step, stdStep = evaluate.evaluate_policy(environment, fqi, nEvaluation)
+            varSetting.save(regressorN, sizeN, datasetN, repetition, iteration, "score", score)
+            varSetting.save(regressorN, sizeN, datasetN, repetition, iteration, "stdScore", stdScore)
+            varSetting.save(regressorN, sizeN, datasetN, repetition, iteration, "stdScore", step)
+            varSetting.save(regressorN, sizeN, datasetN, repetition, iteration, "stdScore", stdStep)
+
+        # ----------------------------------------------------------------------
         # SAVE FQI STATUS
-        #----------------------------------------------------------------------
-        
+        # ----------------------------------------------------------------------
+
         if iteration % saveEveryNIteration == 0:
             directory = os.path.dirname(pkl_filename)
-            if not os.path.isdir(directory): 
+            if not os.path.isdir(directory):
                 os.makedirs(directory)
-            cPickle.dump({'fqi':fqi,'actualIteration':iteration, "actualRepetition":repetition},open(pkl_filename, "wb"))
+            cPickle.dump({'fqi': fqi, 'actualIteration': iteration, "actualRepetition": repetition},
+                         open(pkl_filename, "wb"))
             dataset.save(ds_filename)
-            
-        
-    actualRepetition = 0
+
+    actualIteration = 0
--- examples/jsonWriter.py
+++ examples/jsonWriter.py
@@ -3,24 +3,29 @@ Here we easily organize a json
 """
 
 from __future__ import print_function
+import json
 
 def ask(message, err_message, function):
+    # type: (string, string, lambda) -> object
     while True:
         try:
             ret = raw_input(message)
             ret = function(ret)
+            print("OK!")
             break
         except:
             print(err_message)
     return ret
     
 def positiveNumber(x):
+
     x = int(x)
     if x > 0:
         return x
     raise Exception("Number should be positive")
     
 def getMDP(name):
+
     if (name == 'CarOnHill' 
         or name == 'SwingUpPendulum'
         or name == 'Acrobot'
@@ -34,7 +39,8 @@ def getMDP(name):
     else:
         raise Exception("Insert a valid Environment name")
 
-def getRegressor(name):
+def getRegressor(name, errorMessage=""):
+
     if (name == 'ExtraTree' 
         or name == 'ExtraTreeEnsemble'
         or name == 'MLP'
@@ -43,8 +49,10 @@ def getRegressor(name):
         or name == "LinearEnsemble"):
             return name
     else:
-        raise Exception("Insert a valid Environment name")
-        
+        raise Exception(errorMessage)
+
+yesQuestion = lambda x: str(x).capitalize() in ["Y","YE","YES",1,"1"]
+
 nLine=10
 
 print("Welcome in JsonWriter 1.0")
@@ -69,19 +77,34 @@ jsonFile["experimentSetting"] = {
         "loadPath":ask("Select the load path ", "loadPath should be a string", str ),
         "savePath":ask("Select the load path ", "loadPath should be a string", str ),
         "datasets":ask("Select the number of datasets ", "Number of dataset should be integer and positive", positiveNumber),
-        "nExperiments":ask("Select the number of repetition on each dataset ", "Number of dataset should be integer and positive", positiveNumber)
+        "nExperiments":ask("Select the number of repetition on each dataset ", "Number of dataset should be integer and positive", positiveNumber),
+        "evaluations":{
+            "everyNIterations":ask("Select how often would you like to evaluate the policy found", "Please insert a positive number", positiveNumber),
+            "nEvaluations":ask("Select how many episodes would you like to run on your environment", "Please insert a positive integer number", positiveNumber)
+        }
     }
+
+newSize = True
+sizes = []
+while newSize:
+
+    size = ask("Insert a size (number of episodes) for the experiment","Insert a positive integer number please", positiveNumber)
+    sizes.append(size)
+    if ask("Would you like to insert another size?", "Just type 'y' to say Yes",yesQuestion):
+        newSize = False
+
+jsonFile["experimentSetting"]["sizes"] = sizes
 
 newRegressor = True
 regressorList = []
 while newRegressor:
-    regressorName = getRegressor("Insert the name of the regressor you wish: ", """Please insert a name from the following list
+    regressorName = getRegressor('Insert the name of the regressor you wish: ', """Please insert a name from the following list
 \tExtraTree
 \tExtraTreeEnsemble
 \tLinear
 \tLinearEnsemble
 \tMLP
-\tMLPEnsemble    
+\tMLPEnsemble
 """)
     if regressorName == "MLP" or regressorName == "MLPEnsemble":
         regressor = {
@@ -95,8 +118,8 @@ while newRegressor:
         regressor = {
             "modelName": regressorName,
             "nEstimators": ask("Define the number of trees you would like to have: ", "Insert a positive numer", positiveNumber),
-            "min_samples_split": ask("Define minSamplesSplit: ", "Insert a positive numer", positiveNumber),
-            "min_samples_leaf": ask("Define minSamplesLeaf: ", "Insert a positive numer", positiveNumber)
+            "minSamplesSplit": ask("Define minSamplesSplit: ", "Insert a positive numer", positiveNumber),
+            "minSamplesLeaf": ask("Define minSamplesLeaf: ", "Insert a positive numer", positiveNumber)
         }
     elif regressorName == "Linear" or regressorName == "LinearEnsemble":
         regressor =  {
@@ -108,5 +131,47 @@ while newRegressor:
         }
     else:
         raise Exception("The regressor name is not defined")
-    
+
+    if not raw_input("Would you like to insert another regerssor?").capitalize() in ["Y", "YE", "YES", 1, "1"]:
+        newRegressor = False
+
     regressorList.append(regressor)
+
+
+
+print("-"*nLine + "RLAlgorithm Section - FQI Default" + "-"*nLine )
+lrAlgorithm = {
+        "algorithm":"FQI",
+        "nIterations":ask("Prompt the number of iterations of your desire: ", "Please insert a positive integer number", positiveNumber),
+        "gamma":ask("What's the gamma? ","Insert a float (between 0 and 1)", float),
+        "verbosity":ask("Would you like that FQI will be verbose (Y|n)?","Say Y to say yes", yesQuestion),
+        "horizon":ask("Insert the horizon", "Insert a positive integer number ", yesQuestion),
+        "scaled":ask("Would you like to scale the dataset? (Y|n)","To say yes just type y", yesQuestion)
+    }
+
+if ask("Would you like to have experience replay (Y|n)", "Say y to say yes", yesQuestion):
+    lrAlgorithm["replayExperience"] = {
+            "everyNIterations":ask("how many iterations would you like to collect experience?","Please insert positive integer", positiveNumber),
+            "nExperience":ask("How many episodes would you like to collect?", "Please insert a positive integer", positiveNumber)
+        }
+
+jsonFile["regressors"] = regressorList
+jsonFile["rlAlgorithm"] = lrAlgorithm
+
+print("-"*nLine + "\nSupervised Algorithm Session\n" + "-"*nLine)
+
+
+jsonFile["supervisedAlgorithm"] = {
+        "nEpochs":ask("How many epochs would you like?", "Positive integer required", yesQuestion),
+        "batchSize":ask("What's the batch size?", "Positive integer required", yesQuestion),
+        "validationSplit":ask("What's the validation split (from 0 to 1)?", "Float between 0 e 1", float ),
+        "verbosity":ask("Would you like Supervised Algorithm to be verbose(Y|n)?", "Just type 'y' for yes", yesQuestion)
+    }
+
+jsonFile["mdp"] = {
+        "mdp_name":mdpName
+    }
+
+fileName =  ask("What is the name of your new json?" , "Just type a string", str)
+with open("results/" + fileName + ".json", 'w') as fp:
+    json.dump(camelize(json_file), fp)
