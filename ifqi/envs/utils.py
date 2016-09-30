49ec47f553ef22402e6ee6127b290dd334c1fc96
matteo <matteo.pirotta@polimi.it>
2016-09-30 10:27:45 +0200

    Updated

--- ifqi/envs/utils.py
+++ ifqi/envs/utils.py
@@ -7,9 +7,9 @@ def getSpaceInfo(env):
     
     state_space = env.observation_space
     if isinstance(state_space, 'spaces.Box'):
-        statedim = state_space.shape[0]
-    elif isinstace(state_space, 'spaces.Discrete'):
-        statedim = 1
+        stateDim = state_space.shape[0]
+    elif isinstance(state_space, 'spaces.Discrete'):
+        stateDim = 1
     elif isinstance(state_space, 'DiscreteValued'):
         stateDim = state_space.action_dim()
     else:
@@ -18,11 +18,11 @@ def getSpaceInfo(env):
     action_space = env.action_space
     if isinstance(action_space, 'spaces.Box'):
         actionDim = action_space.shape[0]
-    elif isinstace(action_space, 'spaces.Discrete'):
+    elif isinstance(action_space, 'spaces.Discrete'):
         actionDim = 1
     elif isinstance(action_space, 'DiscreteValued'):
         actionDim = action_space.action_dim()
     else:
         raise NotImplementedError
     
-    return statedim, actiondim
+    return stateDim, actionDim
