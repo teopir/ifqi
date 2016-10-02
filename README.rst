1acaf2ab8e6f7cdabef6722b475da50973a1fd53
matteo <matteo.pirotta@polimi.it>
2016-10-02 19:05:28 +0200

    Added setup for packaging and distribute the lib

---
+++ README.rst
@@ -0,0 +1,29 @@
+iFQI
+******
+
+**iFQI is a toolkit for solving Reinforcement Learning problem using Fitted Q-Iteration**
+
+.. contents:: **Contents of this document**
+   :depth: 2
+
+Installation
+============
+
+You can perform a minimal install of ``ifqi`` with:
+
+..code:: shell
+
+	git clone https://github.com/teopir/ifqi.git
+	cd ifqi
+	pip install -e .
+
+Installing everything
+---------------------
+
+To install the whole set of features, you will need additional packages installed.
+You can install everything by running ``pip install -e '.[all]'``.
+
+
+What's new
+----------
+- 2016-XX-YY: Initial release
---
+++ setup.py
@@ -0,0 +1,98 @@
+"""A setuptools based setup module.
+
+See:
+https://packaging.python.org/en/latest/distributing.html
+https://github.com/pypa/sampleproject
+"""
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+import sys
+
+here = path.abspath(path.dirname(__file__))
+name = 'ifqi'
+
+# Get the long description from the README file
+requires_list = []
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    for line in f:
+        requires_list.append(str(line))
+
+# get the version from the library
+sys.path.insert(0, path.join(path.dirname(__file__), name))
+from version import VERSION
+
+# specific dependencies for modules
+extras = {
+    'dev': ['check-manifest'],
+    'test': ['coverage'],
+    'nn': ['keras', 'theano']
+}
+
+# Meta dependency groups.
+all_deps = []
+for group_name in extras:
+    all_deps += extras[group_name]
+extras['all'] = all_deps
+
+setup(
+    name=name,
+
+    # Versions should comply with PEP440.  For a discussion on single-sourcing
+    # the version across setup.py and the project code, see
+    # https://packaging.python.org/en/latest/single_source_version.html
+    version=VERSION,
+
+    description='A toolkit for Fitted Q-Iteration',
+
+    # The project's main homepage.
+    url='https://github.com/teopir/' + name,
+
+    # Author details
+    author='PoliMI',
+    author_email='',
+
+    # Choose your license
+    license='',
+
+    # You can just specify the packages manually here if your project is
+    # simple. Or you can use find_packages().
+    packages=[package for package in find_packages()
+              if package.startswith(name)],
+
+    zip_safe=False,
+
+    # Alternatively, if you want to distribute just a my_module.py, uncomment
+    # this:
+    #   py_modules=["my_module"],
+
+    # List run-time dependencies here.  These will be installed by pip when
+    # your project is installed. For an analysis of "install_requires" vs pip's
+    # requirements files see:
+    # https://packaging.python.org/en/latest/requirements.html
+    install_requires=requires_list,
+
+    # List additional groups of dependencies here (e.g. development
+    # dependencies). You can install these using the following syntax,
+    # for example:
+    # $ pip install -e .[dev,test,all]
+    extras_require=extras,
+
+    # If there are data files included in your packages that need to be
+    # installed, specify them here.  If using Python 2.6 or less, then these
+    # have to be included in MANIFEST.in as well.
+    # package_data={
+    #     'sample': ['package_data.dat'],
+    # },
+
+    # Although 'package_data' is the preferred approach, in some case you may
+    # need to place data files outside of your packages. See:
+    # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files # noqa
+    # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
+    # data_files=[('my_data', ['data/data_file'])],
+    tests_require=['nose2'],
+
+)
