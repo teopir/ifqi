a578e9d6f049dbcf44378ba46fcbecb9082d3713
carloderamo <carlo.deramo@gmail.com>
2016-10-11 10:46:40 +0200

    remove useless files and fix utils.py

--- ifqi/envs/utils.py
+++ ifqi/envs/utils.py
@@ -4,25 +4,25 @@ import gym.spaces as spaces
 from ifqi.utils.spaces.discretevalued import  DiscreteValued
 
 def getSpaceInfo(env):
-    
+
     state_space = env.observation_space
-    if isinstance(state_space, 'spaces.Box'):
+    if isinstance(state_space, spaces.Box):
         stateDim = state_space.shape[0]
-    elif isinstance(state_space, 'spaces.Discrete'):
+    elif isinstance(state_space, spaces.Discrete):
         stateDim = 1
-    elif isinstance(state_space, 'DiscreteValued'):
-        stateDim = state_space.action_dim()
+    elif isinstance(state_space, DiscreteValued):
+        stateDim = state_space.value_dim
     else:
         raise NotImplementedError
-    
+
     action_space = env.action_space
-    if isinstance(action_space, 'spaces.Box'):
+    if isinstance(action_space, spaces.Box):
         actionDim = action_space.shape[0]
-    elif isinstance(action_space, 'spaces.Discrete'):
+    elif isinstance(action_space, spaces.Discrete):
         actionDim = 1
-    elif isinstance(action_space, 'DiscreteValued'):
-        actionDim = action_space.action_dim()
+    elif isinstance(action_space, DiscreteValued):
+        actionDim = action_space.value_dim
     else:
         raise NotImplementedError
-    
+
     return stateDim, actionDim
