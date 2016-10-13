b08aa433201da3452af39c09c15b9dcc59d22c93
SamuelePolimi <samuele.tosatto@gmail.com>
2016-10-14 00:39:15 +0200

    plot updated but still not finished

--- examples/plot.py
+++ examples/plot.py
@@ -11,7 +11,58 @@ import os.path
 from ifqi.experiment import Experiment
 import matplotlib.pyplot as plt
 from examples.variableLoadSave import  ExperimentVariables
+import json
+import matplotlib as mpl
+from mpl_toolkits.mplot3d import Axes3D
 import re
+
+"""example of data
+data={mlp:{
+iteration:[1,2,3,4,5]
+mean:[1,2,3,4,5],
+std:None
+},
+wide:{
+mean:[2,3,1,4],
+std:[0.5,0.5,0.6,0.6]
+}}
+"""
+
+
+def multiOneDimPlot(data, xlabel, ylabel, title, path):
+    color = ["r", "g", "b", "k"]
+    imgName = os.path.realpath(path)
+    i = 0
+    hand = []
+    min_ = np.inf
+    max_ = -np.inf
+    for dic in data:
+        dict_ = data[dic]
+        name = dict_["name"]
+        mean = dict_["mean"]
+        std = dict_["std"]
+
+        if min(mean) < min_:
+            min_ = min(mean)
+        if max(mean) > max_:
+            max_ = max(mean)
+
+        if (len(std) != 0):
+            plt.errorbar(dict_["iteration"], mean, color=color[i], yerr=std, lw=1, label=name)
+        hand.append(plt.plot(dict_["iteration"], mean, color=color[i], lw=1, label=name)[0])
+
+        plt.ylabel(ylabel)
+        plt.xlabel(xlabel)
+        plt.title(title)
+
+        i += 1
+        i %= len(color)
+
+    plt.legend(handles=hand, loc=4)
+    boundaries = max(abs(min_) * 0.1, 0.1 * abs(max_))
+    plt.ylim(min_ - boundaries, max_ + boundaries)
+    plt.savefig(imgName)
+    plt.show()
 
 def ask(message, err_message, function):
     # type: (string, string, lambda) -> object
@@ -40,62 +91,56 @@ get = expVar.getOverallSituation(variable)
 nLines = len(get)
 print(str(nLines) + " lines are found.")
 
+for s in expVar.sizeLoaded:
+    data = {}
+    for reg in expVar.regressorLoaded:
+        key = (reg, s)
+        data[key] = {}
+        size = exp.config['experimentSetting']['sizes'][s]
+        data[key]["name"] = str(exp.getModelName(reg)) + "_" + str(size)
+        data[key]["iteration"] = get[key][0]
+        data[key]["mean"] = get[key][1]
+        data[key]["std"] = get[key][2]
+    filepath = "plot/" + experimentPath + "/" + "size" + str(size)+".jpg"
+    directory =os.path.dirname(filepath)
+    if not os.path.isdir(directory): os.makedirs(directory)
+    multiOneDimPlot(data,"iterations","score","size=" + str(size),filepath)
+
+get = expVar.getSizeLines("score")
 data = {}
-for key in get:
-    #Ask if he/she whants to add the line to the plot
+for reg in expVar.regressorLoaded:
+    key=reg
     data[key] = {}
-    data[key]["name"] = str(exp.getModelName(key[0])) + "_" + str(key[1])
-    data[key]["iteration"] = get[key][0]
+    data[key]["name"] = str(exp.getModelName(reg))
+    data[key]["iteration"] = [exp.config['experimentSetting']['sizes'][x] for x in get[key][0]]
     data[key]["mean"] = get[key][1]
     data[key]["std"] = get[key][2]
 
-            
-"""example of data
-data={mlp:{
-iteration:[1,2,3,4,5]
-mean:[1,2,3,4,5],
-std:None
-},
-wide:{
-mean:[2,3,1,4],
-std:[0.5,0.5,0.6,0.6]
-}}
-"""
-def multiOneDimPlot(data,xlabel,ylabel,title,path):
-    color = ["r","g","b","k"]
-    imgName = os.path.realpath(path)
-    i=0
-    hand = []
-    min_ = np.inf
-    max_ = -np.inf
-    for dic in data:
-        dict_ = data[dic]
-        name = dict_["name"]
-        mean = dict_["mean"]
-        std = dict_["std"]
-        
-        if min(mean) < min_:
-            min_ = min(mean)
-        if max(mean) > max_:
-            max_ = max(mean)
-        
-        if(len(std)!=0):
-            plt.errorbar(dict_["iteration"],mean,color=color[i],yerr=std,lw=1,label=name)
-        hand.append(plt.plot(dict_["iteration"],mean,color=color[i],lw=1,label=name)[0])
+filepath = "plot/" + experimentPath + "/" + "sizeview.jpg"
+directory =os.path.dirname(filepath)
+if not os.path.isdir(directory): os.makedirs(directory)
+multiOneDimPlot(data,"sizes","score","SizeView",filepath)
 
-        plt.ylabel(ylabel)
-        plt.xlabel(xlabel)
-        plt.title(title)
-        
-        i+=1
-        i%=len(color)
-    
-    plt.legend(handles=hand)
-    plt.ylim(min_ - abs(min_) * 0.1 , max_ + 0.1 * abs(max_))   
-    plt.savefig(imgName)
+
+if input("Would you like to plot Q [True]?"):
+    environment = exp.getMDP(1)
+    path = raw_input("path of the file")
+    tabular_Q = np.load(path)
+    l = []
+    xs = np.linspace(-environment.max_pos, environment.max_pos, 60)
+    us = np.linspace(-environment.max_action, environment.max_action, 50)
+
+
+    # print(tabular_Q.shape)
+    # print(tabular_Q)
+    fig = plt.figure()
+    ax = fig.gca(projection='3d')
+    ax.scatter(tabular_Q[:, 0], tabular_Q[:, 1], tabular_Q[:, 2])
+    filepath = "plot/" + experimentPath + "/" + "QRegressor.jpg"
+    directory = os.path.dirname(filepath)
+    if not os.path.isdir(directory): os.makedirs(directory)
+    plt.savefig(filepath)
     plt.show()
-
-multiOneDimPlot(data,"iterations","score","ciao","prova.jpg")
 
 path = "prova.jpg"
 if input("Would you like to save [True]?"):
