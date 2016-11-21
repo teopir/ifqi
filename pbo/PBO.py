6e4ab2c8ce4ca7b70078cc87890f11d1a62b27e2
carloderamo <carlo.deramo@gmail.com>
2016-11-21 17:41:11 +0100

    first version of pbo

---
+++ pbo/PBO.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+
+class PBO:
+    def __init__(self, theta, actions, training_set, gamma=1):
+        self.theta = theta
+        self._actions = actions
+        self._training_set = training_set
+        self.gamma = gamma
+
+    def fitness(self, rho):
+        n_samples = self._training_set.shape[0]
+        result = 0
+        for i in range(n_samples):
+            m = 0
+            s = self._training_set[i, 0]
+            a = self._training_set[i, 1]
+            r = self._training_set[i, 2]
+            ns = self._training_set[i, 3]
+            s, a, r, ns = self._training_set[i, :4]
+
+            Q = self.Q(ns, self._actions, self.theta)
+            Q = Q * (1 - self._training_set[i, -2])  # absorbing states
+            result += self.Q(s, a, self.f(rho)) - r - self.gamma * np.max(Q)
+
+        print(result ** 2 / n_samples)
+        return result ** 2 / n_samples
+
+    def Q(self, s, a, theta):
+        k, b = theta
+        return b - (a - k * s) ** 2
+
+    def f(self, rho):
+        return rho * self.theta
---
+++ pbo/simple_run_pbo.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+from ifqi import envs
+from ifqi.evaluation import evaluation
+from PBO import PBO
+from pybrain.optimization import ExactNES
+
+mdp = envs.LQG1D()
+state_dim, action_dim, reward_dim = envs.get_space_info(mdp)
+actions = np.array([-8, -7, -6, -5, -4, -3, -2.5, -2, -1.5, -1, -.75, -.5,
+                    -.25, 0, .25, .5, .75, 1, 1.5, 2, 2.5, 3, 4, 5, 6, 7, 8])
+dataset = evaluation.collect_episodes(mdp, n_episodes=200)
+
+theta = np.array([1., 0.])
+rho = np.array([1., 0.])
+
+pbo = PBO(theta, actions, dataset)
+
+epsilon = 1e-5
+delta = np.inf
+while delta > epsilon:
+    optimizer = ExactNES(pbo.fitness, rho, minimize=True,
+                         desiredEvaluation=1e-8)
+    rho, score = optimizer.learn()
+
+    old_theta = pbo.theta
+    pbo.theta = pbo.f(rho)
+    delta = np.sum(pbo.theta - old_theta) ** 2
+
+print(pbo.theta)
