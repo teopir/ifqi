b43a61233091f6d5e9e5f977bafaeba9bbf1c73b
carloderamo <carlo.deramo@gmail.com>
2016-11-22 11:59:55 +0100

    added algorithm interface

--- ifqi/algorithms/algorithm.py
+++ ifqi/algorithms/algorithm.py
@@ -7,11 +7,11 @@ from ifqi.preprocessors.features import select_features
 from ifqi.models.actionregressor import ActionRegressor
 
 """
-This class implements the functions to run Fitted Q-Iteration algorithm.
+Interface for algorithm.
 """
 
 
-class FQI:
+class Algorithm:
     def __init__(self, estimator, state_dim, action_dim,
                  discrete_actions, gamma, horizon,
                  scaled=False, features=None, verbose=False):
@@ -51,7 +51,7 @@ class FQI:
             assert len(self._actions) > 1, \
                 'Error: at least two actions are required'
 
-        self.__name__ = "FittedQIteration"
+        self.__name__ = None
         self._iteration = 0
         self._scaled = scaled
         self._features = select_features(features)
@@ -117,47 +117,7 @@ class FQI:
         self._absorbing = absorbing
 
     def partial_fit(self, sast=None, r=None, **kwargs):
-        """
-        Perform a step of FQI using input data sast and r.
-        Note that if the dataset does not change between iterations, you can
-        provide None inputs after the first iteration.
-
-        Args:
-            sast (numpy.array, None): the input in the dataset
-            r (numpy.array, None): the output in the dataset
-            **kwargs: additional parameters to be provided to the fit function
-            of the estimator
-
-        Returns:
-            sa, y: the preprocessed input and output
-        """
-        # preprocess new data
-        if sast is not None or r is not None:
-            self._preprocess_data(sast, r)
-
-        if self._iteration == 0:
-            if self._verbose > 0:
-                print('Iteration {}'.format(self._iteration + 1))
-
-            y = self._r
-        else:
-            maxq, maxa = self.maxQA(self._snext, self._absorbing)
-
-            if self._verbose > 0:
-                print('Iteration {}'.format(self._iteration + 1))
-
-            if hasattr(self._estimator, 'has_ensembles') \
-               and self._estimator.has_ensembles():
-                    # update estimator structure
-                    self._estimator.adapt(iteration=self._iteration)
-
-            y = self._r + self.gamma * maxq
-
-        self._estimator.fit(self._sa, y.ravel(), **kwargs)
-
-        self._iteration += 1
-
-        return self._sa, y
+        return None
 
     def fit(self, sast, r, **kwargs):
         """
@@ -173,7 +133,7 @@ class FQI:
             sa, y: the preprocessed input and output
         """
         if self._verbose > 0:
-            print("Starting complete FQI ...")
+            print("Starting complete run...")
 
         # reset iteration count
         self.reset()
@@ -257,7 +217,7 @@ class FQI:
 
     def reset(self):
         """
-        Reset FQI.
+        Reset.
         """
         self._iteration = 0
         self._sa = None
---
+++ ifqi/algorithms/fqi/FQI.py
@@ -0,0 +1,64 @@
+from __future__ import print_function
+import numpy as np
+import sklearn.preprocessing as preprocessing
+from numpy.matlib import repmat
+
+from ifqi.algorithms.algorithm import Algorithm
+from ifqi.preprocessors.features import select_features
+from ifqi.models.actionregressor import ActionRegressor
+
+"""
+This class implements the functions to run Fitted Q-Iteration algorithm.
+"""
+
+
+class FQI(Algorithm):
+    def __init__(self, estimator, state_dim, action_dim,
+                 discrete_actions, gamma, horizon,
+                 scaled=False, features=None, verbose=False):
+        super(FQI, self).__init__(estimator, state_dim, action_dim,
+                                  discrete_actions, gamma, horizon, scaled,
+                                  features, verbose)
+
+    def partial_fit(self, sast=None, r=None, **kwargs):
+        """
+        Perform a step of FQI using input data sast and r.
+        Note that if the dataset does not change between iterations, you can
+        provide None inputs after the first iteration.
+
+        Args:
+            sast (numpy.array, None): the input in the dataset
+            r (numpy.array, None): the output in the dataset
+            **kwargs: additional parameters to be provided to the fit function
+            of the estimator
+
+        Returns:
+            sa, y: the preprocessed input and output
+        """
+        # preprocess new data
+        if sast is not None or r is not None:
+            self._preprocess_data(sast, r)
+
+        if self._iteration == 0:
+            if self._verbose > 0:
+                print('Iteration {}'.format(self._iteration + 1))
+
+            y = self._r
+        else:
+            maxq, maxa = self.maxQA(self._snext, self._absorbing)
+
+            if self._verbose > 0:
+                print('Iteration {}'.format(self._iteration + 1))
+
+            if hasattr(self._estimator, 'has_ensembles') \
+               and self._estimator.has_ensembles():
+                    # update estimator structure
+                    self._estimator.adapt(iteration=self._iteration)
+
+            y = self._r + self.gamma * maxq
+
+        self._estimator.fit(self._sa, y.ravel(), **kwargs)
+
+        self._iteration += 1
+
+        return self._sa, y
