ecbd7d322f50b1f963cfee56ed0ca66ada765df9
Matteo <matteo.pirotta@polimi.it>
2016-12-14 17:33:29 +0100

    Fixed minor errors

--- ifqi/algorithms/pbo/gradpbo.py
+++ ifqi/algorithms/pbo/gradpbo.py
@@ -5,14 +5,13 @@ import numpy as np
 
 
 class GradPBO(object):
+    def _compute_max_q(self, s, all_actions, theta):
+        q_values, _ = theano.scan(fn=lambda a, s, theta: self.q_model.model(s, a, theta),
+                                  sequences=[all_actions], non_sequences=[s, theta])
+        return T.max(q_values)
+        # return q_values
 
     def bellman_error(self, s, a, nexts, r, theta, gamma, all_actions):
-        def compute_max_q(s, all_actions, theta):
-            q_values, _ = theano.scan(fn=lambda a, s, theta: self.q_model.model(s, a, theta),
-                                      sequences=[all_actions], non_sequences=[s, theta])
-            return T.max(q_values)
-            # return q_values
-
         # compute new parameters
         # c = self.linear_bellapx(rho, theta)
         c = self.bellman_model.outputs[0]
@@ -20,47 +19,37 @@ class GradPBO(object):
         qbpo = self.q_model.model(s, a, c)
 
         # compute max over actions with old parameters
-        ## qmat = self.linear_qfunction(nexts, all_actions, theta)
-        ## qmax = T.max(qmat, axis=1)
-        qmat, _ = theano.scan(fn=compute_max_q,
+        qmat, _ = theano.scan(fn=self._compute_max_q,
                               sequences=[nexts], non_sequences=[all_actions, theta])
 
         # compute empirical BOP
         v = qbpo - r - gamma * qmat
         # compute error
         err = 0.5 * T.sum(v ** 2)
-        return v
+        return err
 
     def __init__(self, bellman_model, q_model, gamma):
         self.gamma = gamma
-        self.actions = np.array([1, 2, 3])
         s = T.dmatrix()
         a = T.dmatrix()
-        snext = T.dmatrix()
+        s_next = T.dmatrix()
         r = T.dvector()
         all_actions = T.dmatrix()
         self.bellman_model = bellman_model
         self.q_model = q_model
 
-        #define bellman operator
-        theta = bellman_model.inputs
-        assert isinstance(theta, list)
-        assert len(theta) == 1
-        theta = theta[0]
-        bop = bellman_model.outputs[0]
-        self.bopf = theano.function([theta], bop)
+        # define bellman operator
+        assert isinstance(bellman_model.inputs, list)
+        assert len(bellman_model.inputs) == 1
+        theta = bellman_model.inputs[0]
 
-        #bop = self.linear_bellapx(rho, theta)
+        self.berr = self.bellman_error(s, a, s_next, r, theta, self.gamma, all_actions)
+        params = self.bellman_model.trainable_weights
+        self.grad_berr = T.grad(self.berr, params)
 
+        # compile all functions
+        self.bopf = theano.function([theta], bellman_model.outputs[0])
         q = self.q_model.model(s, a, theta)
         self.qf = theano.function([s, a, theta], q)
-
-
-        params = self.bellman_model.trainable_weights
-
-        self.berr = self.bellman_error(s, a, snext, r, theta, self.gamma, all_actions)
-        #self.grad_berr = T.grad(T.sum(self.berr), params)
-        self.berrf = theano.function([s, a, snext, r, theta, all_actions], self.berr, on_unused_input='ignore')
-        #self.grad_berrf = theano.function([s, a, snext, r, theta, all_actions], self.grad_berr, on_unused_input='ignore')
-
-
+        self.berrf = theano.function([s, a, s_next, r, theta, all_actions], self.berr)
+        self.grad_berrf = theano.function([s, a, s_next, r, theta, all_actions], self.grad_berr)
