e36f88dd594c86e24a78366412ff3f1314142e12
danielegrattarola <daniele.grattarola@gmail.com>
2016-12-15 16:07:15 +0100

    Added correlation matrix heatmap of encoded features and coordinates in build_dataset

--- ifqi/algorithms/selection/feature_extraction/build_dataset.py
+++ ifqi/algorithms/selection/feature_extraction/build_dataset.py
@@ -1,4 +1,12 @@
-import random, argparse, numpy as np, progressbar
+import random, argparse, numpy as np, progressbar, pandas as pd
+#############################################################################
+# Keep this import sequence to correctly create heatmaps on a headless server
+#############################################################################
+import matplotlib as mpl
+mpl.use('Agg')
+import matplotlib.pyplot as plt
+import seaborn as sns
+#############################################################################
 from Logger import Logger
 from joblib import Parallel, delayed
 from helpers import flat2gen, onehot_encode
@@ -9,6 +17,7 @@ parser.add_argument('--path', type=str, default='data/model.h5', help='path to t
 parser.add_argument('-v', '--video', action='store_true', help='display video output')
 parser.add_argument('-e', '--encode', action='store_true', help='save a SARS dataset with the encoded state features')
 parser.add_argument('-i', '--images', action='store_true', help='save images of states with a SARS csv')
+parser.add_argument('--heatmap', action='store_true', help='plot correlation matrix of features and coordinates')
 parser.add_argument('-d', '--debug', action='store_true', help='run in debug mode (no output files)')
 parser.add_argument('--episodes', type=int, default=1000, help='number of episodes to run')
 parser.add_argument('--njobs', type=int, default=1, help='number of threads to use (don\'t set this flag if using GPU)')
@@ -18,10 +27,12 @@ assert args.encode != args.images, 'Set exactly one flag between -i and -e'
 
 logger = Logger(debug=args.debug)
 output_csv = 'dataset.csv'
+heatmap_csv = 'heatmap.csv'
 
 if args.encode:
     from Autoencoder import Autoencoder
     logger.to_csv(output_csv, 'S0,S1,S2,S3,S4,S5,A0,A1,A2,A3,R,SS0,SS1,SS2,SS3,SS4,SS5')
+    logger.to_csv(heatmap_csv, 'S0,S1,S2,S3,S4,S5,X,Y')
     AE = Autoencoder((1, 90, 160), load_path=args.path)
 else:
     logger.to_csv(output_csv, 'S,A,R,S1')
@@ -29,7 +40,6 @@ else:
 
 def episode(episode_id):
     global args
-
     env = GridWorldEnv()
     action_space = env.action_space.n
     frame_counter = 0
@@ -63,7 +73,7 @@ def episode(episode_id):
             preprocessed_next_state = np.expand_dims(np.expand_dims(np.asarray(next_state), axis=0), axis=0)
             encoded_next_state = AE.encode(preprocessed_next_state)
             logger.to_csv(output_csv, [i for i in flat2gen([encoded_state[0], onehot_encode(action, action_space), reward, encoded_next_state[0]])])
-
+            logger.to_csv(heatmap_csv, [i for i in flat2gen([encoded_state[0], env.viewer.char_pos[0], env.viewer.char_pos[1]])])
         # Save image of state
         if args.images:
             next_state_id = '%04d_%d' % (episode_id, frame_counter)
@@ -89,3 +99,9 @@ print '\nRunning episodes...'
 n_jobs = args.njobs
 pb = progressbar.ProgressBar(term_width=50)
 Parallel(n_jobs=n_jobs)(delayed(episode)(eid) for eid in pb(xrange(args.episodes)))
+
+# Save heatmap
+if args.heatmap:
+    sns_plot = sns.heatmap(pd.read_csv(heatmap_csv))
+    sns_plot.get_figure().savefig(logger.path + 'heatmap.png')
+
--- ifqi/algorithms/selection/feature_extraction/train_autoencoder.py
+++ ifqi/algorithms/selection/feature_extraction/train_autoencoder.py
@@ -1,4 +1,4 @@
-import argparse
+import argparse, progressbar
 from Autoencoder import Autoencoder
 from helpers import *
 from Logger import Logger
