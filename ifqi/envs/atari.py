6763d11aeba5eb4abedae718c50a6f4f0b7a2cc3
carloderamo <carlo.deramo@gmail.com>
2017-01-10 11:39:19 +0100

    fix in atari env

--- ifqi/envs/atari.py
+++ ifqi/envs/atari.py
@@ -1,8 +1,8 @@
 import gym
-import math
 import numpy as np
-from gym.utils import seeding
 from .environment import Environment
+from gym.utils import seeding
+from PIL import Image
 
 
 class Atari(Environment):
@@ -15,9 +15,11 @@ class Atari(Environment):
         'video.frames_per_second': 15
     }
 
-    def __init__(self, name='BreakoutDeterministic-v3'):
-        self.horizon = 400
+    def __init__(self, name='BreakoutDeterministic-v3', grayscale=True):
+        self.IMG_SIZE = (84, 110)
         self.gamma = 0.99
+
+        self.grayscale = grayscale
 
         self.env = gym.make(name)
         self.action_space = self.env.action_space
@@ -33,14 +35,23 @@ class Atari(Environment):
         return [seed]
 
     def reset(self):
-        state = self.env.reset()
+        state = self._preprocess_observation(self.env.reset())
         self.env.state = np.array([state, state, state, state])
         return self.get_state()
 
     def step(self, action):
         current_state = self.get_state()
-        new_state = self.env.step(int(action))
-        return np.append(current_state[1:], new_state)
+        obs = self._preprocess_observation(self.env.step(int(action)))
+        return self._get_next_state(current_state, obs)
 
     def get_state(self):
         return self.env.state
+
+    def _preprocess_observation(self, obs):
+        image = Image.fromarray(obs, 'RGB').convert('L').resize(self.IMG_SIZE)
+        return np.asarray(image.getdata(), dtype=np.uint8).reshape(image.size[1],
+                                                                   image.size[0])  # Convert to array and return
+
+    def _get_next_state(self, current, obs):
+        # Next state is composed by the last 3 images of the previous state and the new observation
+        return np.append(current[1:], [obs], axis=0)
