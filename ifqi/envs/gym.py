a0671c93ff895f5b733ba6690454f735097e2763
carloderamo <carlo.deramo@gmail.com>
2017-01-11 09:56:19 +0100

    gym env added

--- ifqi/envs/gym.py
+++ ifqi/envs/gym.py
@@ -1,13 +1,11 @@
 import gym
-import math
-import numpy as np
 from gym.utils import seeding
 from .environment import Environment
 
 
-class CartPole(Environment):
+class Gym(Environment):
     """
-    The CartPole environment.
+    The openAI Gym environment.
 
     """
     metadata = {
@@ -15,11 +13,8 @@ class CartPole(Environment):
         'video.frames_per_second': 15
     }
 
-    def __init__(self):
-        self.horizon = 400
-        self.gamma = 0.99
-
-        self.env = gym.make('CartPole-v0')
+    def __init__(self, name):
+        self.env = gym.make(name)
         self.action_space = self.env.action_space
         self.action_space.values = range(self.action_space.n)
         self.observation_space = self.env.observation_space
@@ -31,13 +26,6 @@ class CartPole(Environment):
     def seed(self, seed=None):
         self.np_random, seed = seeding.np_random(seed)
         return [seed]
-
-    def reset(self, state=None):
-        if state is None:
-            return self.env.reset()
-        else:
-            self.env.state = state
-            return self.get_state()
 
     def step(self, action):
         return self.env.step(int(action))
