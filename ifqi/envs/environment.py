0df97dab97a5cc0251fd9a2ca42c36db090ebf63
carloderamo <carlo.deramo@gmail.com>
2017-01-11 11:56:10 +0100

    minor changes in seeding

--- ifqi/envs/environment.py
+++ ifqi/envs/environment.py
@@ -4,6 +4,5 @@ from gym.utils import seeding
 
 class Environment(gym.Env):
     def seed(self, seed=None):
-        if seed is not None:
-            self.np_random, seed = seeding.np_random(seed)
-            return [seed]
+        self.np_random, seed = seeding.np_random(seed)
+        return [seed]
