Major changes in gradient BO methods - added missing (1 - absorbing) term in gradpbo - fixed interface of gengradfqi

examples/simple_run_gradpbo.py
@@ -131,8 +131,8 @@ res = rho_regressor.model(theta)
 # rho_regressor.fit(None, None)
 ##########################################
 
-def terminal_evaluation(old_theta, new_theta):
-    if increment_base_termination(old_theta, new_theta, 2, 1e-2):
+def terminal_evaluation(old_theta, new_theta, tol_theta=1e-2):
+    if increment_base_termination(old_theta, new_theta, 2, tol_theta):
         estimator = LQG_Q()
         estimator.omega = new_theta[0]
         agent = Algorithm(estimator, state_dim, action_dim,
@@ -161,9 +161,10 @@ pbo = GradPBO(bellman_model=rho_regressor,
               steps_per_theta_update=None,
               verbose=1,
               norm_value=NORM_VALUE,
-              independent=INDEPENDENT,
-              # term_condition='theta_improvement')
-              term_condition=terminal_evaluation)
+              independent=INDEPENDENT)
+              # term_condition=lambda v1, v2:
+              # increment_base_termination(v1,v2,2,tol=1e-2))
+              #term_condition=lambda v1, v2: terminal_evaluation(v1,v2,1e-1))
 
 
 def tmetric(theta):
@@ -171,16 +172,17 @@ def tmetric(theta):
     return q_regressor.get_k(t)
 
 
-state, actions, reward, next_states = split_dataset(dataset,
+state, actions, reward, next_states, absorbing = split_dataset(dataset,
                                                     state_dim=state_dim,
                                                     action_dim=action_dim,
                                                     reward_dim=reward_dim)
 
 theta0 = np.array([6., 10.001], dtype='float32').reshape(1, -1)
 # theta0 = np.array([16., 10.001], dtype='float32').reshape(1, -1)
-history = pbo.fit(state, actions, next_states, reward, theta0,
+history = pbo.fit(state, actions, next_states, reward, absorbing, theta0,
                   batch_size=10, nb_epoch=EPOCH,
                   theta_metrics={'k': tmetric})
+
 ##########################################
 # Evaluate the final solution
 initial_states = np.array([[1, 2, 5, 7, 10]]).T
@@ -202,7 +204,8 @@ plt.xlabel('b')
 plt.ylabel('k')
 plt.colorbar()
 plt.savefig(
-    'LQG_MLP_{}_evaluated_weights_incremental_{}_activation_{}_steps_{}.png'.format(
+    'LQG_MLP_{}_evaluated_weights_incremental_{}_'
+    'activation_{}_steps_{}.png'.format(
         q_regressor.name(), INCREMENTAL,
         ACTIVATION, STEPS_AHEAD),
     bbox_inches='tight')
@@ -241,7 +244,8 @@ plt.title('Application of Bellman operator')
 plt.xlabel('b')
 plt.ylabel('k')
 plt.savefig(
-    'LQG_MLP_{}_bpo_application_incremental_{}_activation_{}_steps_{}.png'.format(
+    'LQG_MLP_{}_bpo_application_incremental_{}_'
+    'activation_{}_steps_{}.png'.format(
         q_regressor.name(), INCREMENTAL,
         ACTIVATION, STEPS_AHEAD),
     bbox_inches='tight')

ifqi/algorithms/generalizedfqi.py
@@ -2,27 +2,14 @@ from __future__ import print_function
 import theano
 import theano.tensor as T
 import numpy as np
-import copy
 import time
+from six import iteritems
 
 from keras.engine.training import slice_X, batch_shuffle, make_batches, \
     standardize_input_data, check_array_lengths
 from keras import optimizers
-from keras import callbacks as cbks
 
 from .algorithm import Algorithm
-
-
-class PBOHistory(cbks.Callback):
-    def on_train_begin(self, logs={}):
-        self.epoch = []
-        self.batch = []
-        self.hist = {}
-
-    def on_batch_end(self, batch, logs={}):
-        for k in self.params['metrics'] + ['theta']:
-            if k in logs:
-                self.hist.setdefault(k, []).append(logs[k])
 
 
 class GenGradFQI(Algorithm):
@@ -54,10 +41,12 @@ class GenGradFQI(Algorithm):
                  norm_value=2, update_theta_every=1, horizon=10,
                  verbose=0):
         super(GenGradFQI, self).__init__(estimator, state_dim, action_dim,
-                                         discrete_actions, gamma, horizon, verbose)
+                                         discrete_actions, gamma, horizon,
+                                         verbose)
         # save MDP information
         self.norm_value = norm_value
-        self.update_theta_every = update_theta_every if update_theta_every > 0 else -1
+        self.update_theta_every = update_theta_every if update_theta_every > \
+                                                        0 else -1
 
         # create theano variables
         self.T_Y = T.dvector()
@@ -83,14 +72,16 @@ class GenGradFQI(Algorithm):
         self.optimizer = optimizers.get(optimizer)
 
         # validate input data (the output is a list storing the validated input)
-        self.discrete_actions = standardize_input_data(discrete_actions, ['discrete_actions'],
-                                                       [(None,
-                                                         self.action_dim)] if self.action_dim is not None else None,
-                                                       exception_prefix='discrete_actions')
+        self.discrete_actions = standardize_input_data(
+            discrete_actions,
+            ['discrete_actions'],
+            [(None, self.action_dim)] if self.action_dim is not None else None,
+            exception_prefix='discrete_actions')
 
     def _make_train_function(self):
         """
-        Construct the python train function from theano to be used in the fit process
+        Construct the python train function from theano to be used
+        in the fit process
         Returns:
             None
         """
@@ -99,16 +90,19 @@ class GenGradFQI(Algorithm):
             start = time.time()
             inputs = self._estimator.inputs + [self.T_Y]
 
-            training_updates = self.optimizer.get_updates(self._estimator.trainable_weights,
-                                                          {}, self.fqi_loss)
+            training_updates = self.optimizer.get_updates(
+                self._estimator.trainable_weights,
+                {}, self.fqi_loss)
 
             # returns loss and metrics. Updates weights at each call.
-            self.train_function = theano.function(inputs, [self.fqi_loss], updates=training_updates, name="trainer")
+            self.train_function = theano.function(inputs, [self.fqi_loss],
+                                                  updates=training_updates,
+                                                  name="trainer")
             print('compiled in {}s'.format(time.time() - start))
 
     def fit(self, sast, r,
-            batch_size=32, nb_epoch=10, verbose=1, callbacks=[],
-            validation_split=0., validation_data=None, shuffle=True, theta_metrics={}):
+            batch_size=32, nb_epoch=10, shuffle=True,
+            theta_metrics={}):
         """
 
         Args:
@@ -140,108 +134,14 @@ class GenGradFQI(Algorithm):
         Returns:
             A PBOHistory instance storing train information
         """
-        sast = standardize_input_data(sast, ['sast'], (None, 2 * self.state_dim + self.action_dim + 1),
-                                      check_batch_dim=False, exception_prefix='sast')[0]
+        sast = standardize_input_data(sast, ['sast'], (
+            None, 2 * self.state_dim + self.action_dim + 1),
+                                      exception_prefix='sast')[0]
 
         next_states_idx = self.state_dim + self.action_dim
         sa = sast[:, :next_states_idx]
         s_next = sast[:, next_states_idx:-1]
         absorbing = sast[:, -1]
-
-        # # prepare validation data
-        # if validation_data:
-        #     do_validation = True
-        #     if len(validation_data) == 4:
-        #         val_s, val_a, val_s_next, val_r = validation_data
-        #     elif len(validation_data) == 5:
-        #         val_s, val_a, val_s_next, val_r, val_theta = validation_data
-        #     else:
-        #         raise
-        #
-        #     val_s, val_a, val_s_next, val_r, val_theta = self._standardize_user_data(
-        #         val_s, val_a, val_s_next, val_r, val_theta,
-        #         check_batch_dim=False,
-        #         batch_size=batch_size
-        #     )
-        #     self._make_test_function()
-        #     val_f = self.test_function
-        #     val_ins = val_s + val_a + val_s_next + [val_r]
-        #
-        # elif validation_split and 0. < validation_split < 1.:
-        #     do_validation = True
-        #     split_at = int(len(x[0]) * (1. - validation_split))
-        #     x, val_x = (slice_X(x, 0, split_at), slice_X(x, split_at))
-        #     y, val_y = (slice_X(y, 0, split_at), slice_X(y, split_at))
-        #     sample_weights, val_sample_weights = (
-        #         slice_X(sample_weights, 0, split_at), slice_X(sample_weights, split_at))
-        #     self._make_test_function()
-        #     val_f = self.test_function
-        #     if self.uses_learning_phase and type(K.learning_phase()) is not int:
-        #         val_ins = val_x + val_y + val_sample_weights + [0.]
-        #     else:
-        #         val_ins = val_x + val_y + val_sample_weights
-        # else:
-        #     do_validation = False
-        #     val_f = None
-        #     val_ins = None
-
-        do_validation = False
-        val_f = None
-        val_ins = None
-
-        ins = [sa]
-        self._make_train_function()
-        f = self.train_function
-
-        # prepare display labels
-        out_labels = ['loss']
-
-        if do_validation:
-            callback_metrics = copy.copy(out_labels) + ['val_' + n for n in out_labels]
-        else:
-            callback_metrics = copy.copy(out_labels)
-
-        return self._fit_loop(f, ins, r, s_next, absorbing,
-                              out_labels=out_labels,
-                              batch_size=batch_size, nb_epoch=nb_epoch,
-                              verbose=verbose, callbacks=callbacks,
-                              val_f=val_f, val_ins=val_ins, shuffle=shuffle,
-                              callback_metrics=callback_metrics,
-                              theta_metrics=theta_metrics)
-
-    def _fit_loop(self, f, ins, r, s_next, absorbing, out_labels=[],
-                  batch_size=32, nb_epoch=100, verbose=1, callbacks=[],
-                  val_f=None, val_ins=None, shuffle=True, callback_metrics=[],
-                  theta_metrics={}):
-        do_validation = False
-        if val_f and val_ins:
-            do_validation = True
-            if verbose:
-                print('Train on %d samples, validate on %d samples' %
-                      (ins[0].shape[0], val_ins[0].shape[0]))
-
-        nb_train_sample = ins[0].shape[0]
-        print(nb_train_sample)
-        index_array = np.arange(nb_train_sample)
-
-        history = PBOHistory()
-        callbacks = [history] + callbacks
-        if verbose:
-            callbacks += [cbks.ProgbarLogger()]
-        callbacks = cbks.CallbackList(callbacks)
-        callback_model = self
-
-        callbacks._set_model(callback_model)
-        callbacks._set_params({
-            'batch_size': batch_size,
-            'nb_epoch': nb_epoch,
-            'nb_sample': nb_train_sample,
-            'verbose': verbose,
-            'do_validation': do_validation,
-            'metrics': callback_metrics + [el for el in theta_metrics.keys()]
-        })
-        callbacks.on_train_begin()
-        callback_model.stop_training = False
 
         n_updates = 0
 
@@ -254,18 +154,34 @@ class GenGradFQI(Algorithm):
         # y = np.reshape(r + self.gamma * maxq, (-1, 1))
         y = r + self.gamma * maxq
 
-        ins += [y]
+        ins = [sa, y]
+        self._make_train_function()
+        f = self.train_function
+
+        nb_train_sample = sa.shape[0]
+        index_array = np.arange(nb_train_sample)
+        history = {"theta": []}
+        for k in theta_metrics.keys():
+            history.update({k: []})
 
         for epoch in range(nb_epoch):
-            callbacks.on_epoch_begin(epoch)
             if shuffle == 'batch':
                 index_array = batch_shuffle(index_array, batch_size)
             elif shuffle:
                 np.random.shuffle(index_array)
 
             batches = make_batches(nb_train_sample, batch_size)
-            epoch_logs = {}
             for batch_index, (batch_start, batch_end) in enumerate(batches):
+
+                if hasattr(self._estimator, '_model'):
+                    ltheta = self._model.get_weights()
+                    history["theta"].append(ltheta)
+                else:
+                    ltheta = self._estimator.get_weights()
+                    history["theta"].append(ltheta)
+                for k, v in iteritems(theta_metrics):
+                    history[k].append(v(ltheta))
+
                 batch_ids = index_array[batch_start:batch_end]
                 try:
                     if type(ins[-1]) is float:
@@ -278,22 +194,11 @@ class GenGradFQI(Algorithm):
                                     'If using HDF5 input data, '
                                     'pass shuffle="batch".')
 
-                batch_logs = {}
-                batch_logs['batch'] = batch_index
-                batch_logs['size'] = len(batch_ids)
-                for k in theta_metrics.keys():
-                    batch_logs[k] = theta_metrics[k](self._estimator.get_weights())
-                callbacks.on_batch_begin(batch_index, batch_logs)
-
                 outs = f(*ins_batch)
                 n_updates += 1
 
-                if type(outs) != list:
-                    outs = [outs]
-                for l, o in zip(out_labels, outs):
-                    batch_logs[l] = o
-
-                if self.update_theta_every > 0 and n_updates % self.update_theta_every == 0:
+                if self.update_theta_every > 0 \
+                        and n_updates % self.update_theta_every == 0:
                     maxq, maxa = self.maxQA(s_next, absorbing)
 
                     if hasattr(self._estimator, 'adapt'):
@@ -304,27 +209,8 @@ class GenGradFQI(Algorithm):
                     y = r + self.gamma * maxq
                     ins = [ins[0], y]
 
-                callbacks.on_batch_end(batch_index, batch_logs)
-
-                if batch_index == len(batches) - 1:  # last batch
-                    # validation
-                    if do_validation:
-                        # replace with self._evaluate
-                        val_outs = self._test_loop(val_f, val_ins,
-                                                   batch_size=batch_size,
-                                                   verbose=0)
-                        if type(val_outs) != list:
-                            val_outs = [val_outs]
-                        # same labels assumed
-                        for l, o in zip(out_labels, val_outs):
-                            epoch_logs['val_' + l] = o
-
-            callbacks.on_epoch_end(epoch, epoch_logs)
-
-        callbacks.on_train_end()
-
-        # finally apply the bellman operator K-times to get the final point
         if self._verbose > 1:
-            print('learned theta: {}'.format(self._estimator.get_weights()))
+            print('learned theta: {}'.format(
+                self._estimator.get_weights()))
 
         return history

ifqi/algorithms/pbo/gradpbo.py
@@ -13,10 +13,12 @@ from keras import optimizers
 def increment_base_termination(old_theta, new_theta, norm_value=2, tol=1e-3):
     theta_l = old_theta[0]
     theta_lm1 = new_theta[0]
-    stop = np.linalg.norm(theta_l-theta_lm1, norm_value) < tol
+    stop = np.linalg.norm(theta_l - theta_lm1, norm_value) < tol
     return stop
 
+
 DEFAULT_TERM = {'theta_improvement': increment_base_termination}
+
 
 class GradPBO(object):
     """
@@ -68,13 +70,15 @@ class GradPBO(object):
         self.update_theta_every = update_theta_every if update_theta_every > 0 else -1
         self.verbose = verbose
         self.independent = independent
-        self.steps_per_theta_update = steps_ahead if steps_per_theta_update is None else max(1, steps_per_theta_update)
+        self.steps_per_theta_update = steps_ahead if steps_per_theta_update is None else max(
+            1, steps_per_theta_update)
 
         # create theano variables
         T_s = T.dmatrix()
         T_a = T.dmatrix()
         T_s_next = T.dmatrix()
         T_r = T.dvector()
+        T_absorbing = T.dvector()
         # T_r = T.dmatrix()
         T_discrete_actions = T.dmatrix()
 
@@ -92,21 +96,25 @@ class GradPBO(object):
         # construct (theano) Bellman error
         self.theta_list = [bellman_model.inputs[0]]
         if not independent:
-            self.T_bellman_err, _ = self.k_step_bellman_error(T_s, T_a, T_s_next, T_r,
-                                                              self.theta_list[0],
-                                                              gamma, T_discrete_actions, steps_ahead)
-            # self.T_bellman_err, _ = self.bellman_error(T_s, T_a, T_s_next, T_r, theta, self.gamma, T_discrete_actions)
+            self.T_bellman_err, _ = self.k_step_bellman_error(
+                T_s, T_a, T_s_next, T_r, T_absorbing,
+                self.theta_list[0], gamma, T_discrete_actions, steps_ahead)
             assert len(self.theta_list) == 1
         else:
             self.theta_list += [T.fmatrix(str(ll)) for ll in
-                                range(steps_ahead - 1)]  # theta_0, theta_1, ..., theta_steps
+                                range(
+                                    steps_ahead - 1)]  # theta_0, theta_1, ..., theta_steps
             T_bellman_err = None
             for theta in self.theta_list:
                 if T_bellman_err is None:
-                    T_bellman_err = self.bellman_error(T_s, T_a, T_s_next, T_r, theta, gamma, T_discrete_actions)[0]
+                    T_bellman_err = self.bellman_error(
+                        T_s, T_a, T_s_next, T_r, theta,
+                        gamma, T_discrete_actions)[0]
                 else:
                     T_bellman_err = T_bellman_err + \
-                                    self.bellman_error(T_s, T_a, T_s_next, T_r, theta, gamma, T_discrete_actions)[0]
+                                    self.bellman_error(
+                                        T_s, T_a, T_s_next, T_r, theta,
+                                        gamma, T_discrete_actions)[0]
             self.T_bellman_err = T_bellman_err
             assert len(self.theta_list) == steps_ahead
 
@@ -119,16 +127,16 @@ class GradPBO(object):
         self.T_s_next = T_s_next
         self.T_r = T_r
         self.T_discrete_actions = T_discrete_actions
+        self.T_absorbing = T_absorbing
 
         # get keras optimizer
         self.optimizer = optimizers.get(optimizer)
 
         # validate input data (the output is a list storing the validated input)
-        self.discrete_actions = standardize_input_data(discrete_actions, ['discrete_actions'],
-                                                       [(None,
-                                                         self.action_dim)] if self.action_dim is not None else None,
-                                                       exception_prefix='discrete_actions')
-
+        self.discrete_actions = standardize_input_data(
+            discrete_actions, ['discrete_actions'],
+            [(None, self.action_dim)] if self.action_dim is not None else None,
+            exception_prefix='discrete_actions')
 
         if isinstance(term_condition, str):
             self.term_condition = DEFAULT_TERM[term_condition]
@@ -148,8 +156,9 @@ class GradPBO(object):
             The maximum values of the Q-function in the given state w.r.t. all the discrete actions
 
         """
-        q_values, _ = theano.scan(fn=lambda a, s, theta: self.q_model.model(s, a, theta),
-                                  sequences=[discrete_actions], non_sequences=[s, theta])
+        q_values, _ = theano.scan(
+            fn=lambda a, s, theta: self.q_model.model(s, a, theta),
+            sequences=[discrete_actions], non_sequences=[s, theta])
         return T.max(q_values)
         # return q_values
 
@@ -165,11 +174,13 @@ class GradPBO(object):
         Returns:
             The index of the action that maximixes the Q-function in the given state
         """
-        q_values, _ = theano.scan(fn=lambda a, s, theta: self.q_model.model(s, a, theta),
-                                  sequences=[discrete_actions], non_sequences=[s, theta])
+        q_values, _ = theano.scan(
+            fn=lambda a, s, theta: self.q_model.model(s, a, theta),
+            sequences=[discrete_actions], non_sequences=[s, theta])
         return T.argmax(q_values)
 
-    def bellman_error(self, s, a, nexts, r, theta, gamma, discrete_actions):
+    def bellman_error(self, s, a, nexts, r, absorbing,
+                      theta, gamma, discrete_actions):
         """
         Compute the symbolic expression of the Bellman error.
 
@@ -178,6 +189,7 @@ class GradPBO(object):
             a (theano.matrix): the action matrix (nsamples x state_dim)
             nexts (theano.matrix): the next state matrix (nsamples x state_dim)
             r (theano.vector): the reward vector (nsamples)
+            abserbing (theano.vector): the flag for absorbing states (nsamples)
             theta (theano.matrix): the parameters of the Q-function (1 x n_q_params)
             gamma (float): discount factor
             discrete_actions (theano.matrix): the discrete actions (nactions x action_dim)
@@ -196,10 +208,11 @@ class GradPBO(object):
 
         # compute max over actions with old parameters
         qmat, _ = theano.scan(fn=self._compute_max_q,
-                              sequences=[nexts], non_sequences=[discrete_actions, theta])
+                              sequences=[nexts],
+                              non_sequences=[discrete_actions, theta])
 
         # compute empirical BOP
-        v = qbpo - r - gamma * qmat
+        v = qbpo - r - gamma * qmat * (1. - absorbing)
         # compute error
         if self.norm_value == np.inf:
             err = T.max(v ** 2)
@@ -210,11 +223,14 @@ class GradPBO(object):
             err = T.sum(abs(v) ** self.norm_value) ** (1. / self.norm_value)
         return err, theta_tp1
 
-    def k_step_bellman_error(self, s, a, nexts, r, theta, gamma, discrete_actions, steps):
+    def k_step_bellman_error(self, s, a, nexts, r, absorbing,
+                             theta, gamma, discrete_actions, steps):
         steps = max(1, steps)
-        loss, theta_k = self.bellman_error(s, a, nexts, r, theta, gamma, discrete_actions)
+        loss, theta_k = self.bellman_error(
+            s, a, nexts, r, absorbing, theta, gamma, discrete_actions)
         for _ in range(steps - 1):
-            err_k, theta_k = self.bellman_error(s, a, nexts, r, theta_k, gamma, discrete_actions)
+            err_k, theta_k = self.bellman_error(
+                s, a, nexts, r, absorbing, theta_k, gamma, discrete_actions)
             loss += err_k
         return loss, theta_k
 
@@ -227,16 +243,21 @@ class GradPBO(object):
         if self.train_function is None:
             print('compiling train function...')
             start = time.time()
-            inputs = [self.T_s, self.T_a, self.T_s_next, self.T_r] + self.theta_list + [self.T_discrete_actions]
+            inputs = [self.T_s, self.T_a, self.T_s_next, self.T_r,
+                      self.T_absorbing] + self.theta_list \
+                     + [self.T_discrete_actions]
 
-            training_updates = self.optimizer.get_updates(self.bellman_model.trainable_weights,
-                                                          {}, self.T_bellman_err)
+            training_updates = self.optimizer.get_updates(
+                self.bellman_model.trainable_weights,
+                {}, self.T_bellman_err)
 
             # returns loss and metrics. Updates weights at each call.
-            self.train_function = theano.function(inputs, [self.T_bellman_err], updates=training_updates)
+            self.train_function = theano.function(
+                inputs, [self.T_bellman_err], updates=training_updates)
             print('compiled in {}s'.format(time.time() - start))
 
-    def _standardize_user_data(self, s, a, s_next, r, theta, check_batch_dim=False):
+    def _standardize_user_data(self, s, a, s_next, r, absorbing, theta,
+                               check_batch_dim=False):
         """
 
         Args:
@@ -251,22 +272,29 @@ class GradPBO(object):
             The standardized values (s, a, s_next, r, theta)
 
         """
-        s = standardize_input_data(s, ['s'], [(None, self.state_dim)] if self.state_dim is not None else None,
-                                   exception_prefix='state')
-        a = standardize_input_data(a, ['a'], [(None, self.action_dim)] if self.action_dim is not None else None,
-                                   exception_prefix='action')
+        s = standardize_input_data(
+            s, ['s'],
+            [(None, self.state_dim)] if self.state_dim is not None else None,
+            exception_prefix='state')
+        a = standardize_input_data(
+            a, ['a'],
+            [(None, self.action_dim)] if self.action_dim is not None else None,
+            exception_prefix='action')
         # r = standardize_input_data(r, ['r'], [(None, 1)],
         #                            check_batch_dim=False, exception_prefix='reward')
-        s_next = standardize_input_data(s_next, ['s_next'],
-                                        [(None, self.state_dim)] if self.state_dim is not None else None,
-                                        exception_prefix='state_next')
-        theta = standardize_input_data(theta, ['theta'], (None, self.bellman_model.n_inputs()),
+        s_next = standardize_input_data(
+            s_next, ['s_next'],
+            [(None, self.state_dim)] if self.state_dim is not None else None,
+            exception_prefix='state_next')
+        theta = standardize_input_data(theta, ['theta'],
+                                       (None, self.bellman_model.n_inputs()),
                                        exception_prefix='theta')
         check_array_lengths(s, a, s_next)
-        return s, a, s_next, r, theta
+        return s, a, s_next, r, absorbing, theta
 
-    def fit(self, s, a, s_next, r, theta,
-            batch_size=32, nb_epoch=10, verbose=1, shuffle=True, theta_metrics={}):
+    def fit(self, s, a, s_next, r, absorbing, theta,
+            batch_size=32, nb_epoch=10, shuffle=True,
+            theta_metrics={}):
         """
 
         Args:
@@ -298,21 +326,22 @@ class GradPBO(object):
         Returns:
             A PBOHistory instance storing train information
         """
-        s, a, s_next, r, theta = self._standardize_user_data(
-            s, a, s_next, r, theta,
+        s, a, s_next, r, absorbing, theta = self._standardize_user_data(
+            s, a, s_next, r, absorbing, theta,
             check_batch_dim=False
         )
 
-        all_actions = standardize_input_data(self.discrete_actions, ['all_actions'],
-                                             [(None, self.action_dim)] if self.action_dim is not None else None,
-                                             exception_prefix='discrete_actions')
+        all_actions = standardize_input_data(
+            self.discrete_actions, ['all_actions'],
+            [(None, self.action_dim)] if self.action_dim is not None else None,
+            exception_prefix='discrete_actions')
 
         n_updates = 0
         history = {"theta": [], 'rho': []}
         for k in theta_metrics.keys():
             history.update({k: []})
 
-        ins = s + a + s_next + [r]
+        ins = s + a + s_next + [r, absorbing]
         self._make_train_function()
         f = self.train_function
 
@@ -344,11 +373,12 @@ class GradPBO(object):
 
                 history["theta"].append(theta[0])
                 if hasattr(self.bellman_model, '_model'):
-                    history["rho"].append(self.bellman_model._model.get_weights())
+                    history["rho"].append(
+                        self.bellman_model._model.get_weights())
                 else:
                     history["rho"].append(self.bellman_model.get_weights())
                 for k, v in iteritems(theta_metrics):
-                    history['k'].append(v(theta))
+                    history[k].append(v(theta))
 
                 batch_ids = index_array[batch_start:batch_end]
                 try:
@@ -366,7 +396,8 @@ class GradPBO(object):
                 n_updates += 1
 
                 if self.update_theta_every > 0 and n_updates % self.update_theta_every == 0:
-                    tmp = self.apply_bo(theta[0], n_times=self.steps_per_theta_update)
+                    tmp = self.apply_bo(theta[0],
+                                        n_times=self.steps_per_theta_update)
                     theta = [tmp]
                     for _ in range(len(self.theta_list) - 1):
                         if self.incremental:
@@ -416,9 +447,12 @@ class GradPBO(object):
             # compute max over actions with old parameters
             theta = self.bellman_model.inputs[0]
             idx_max, _ = theano.scan(fn=self._compute_argmax_q,
-                                     sequences=[self.T_s], non_sequences=[self.T_discrete_actions, theta])
+                                     sequences=[self.T_s],
+                                     non_sequences=[self.T_discrete_actions,
+                                                    theta])
             inputs = [self.T_s, theta, self.T_discrete_actions]
-            self.draw_action_function = theano.function(inputs, [self.T_discrete_actions[idx_max]])
+            self.draw_action_function = theano.function(inputs, [
+                self.T_discrete_actions[idx_max]])
 
     def draw_action(self, state, done, flag):
         """
@@ -433,10 +467,12 @@ class GradPBO(object):
         """
         self._make_draw_action_function()
         state = standardize_input_data(state, ['state'],
-                                       [(None, self.state_dim)] if self.state_dim is not None else None,
+                                       [(None,
+                                         self.state_dim)] if self.state_dim is not None else None,
                                        exception_prefix='draw_state')
-        return self.draw_action_function(state[0], self.learned_theta_value, self.discrete_actions[
-            0])  # we take index zero since they are lists of numpy matrices
+        return self.draw_action_function(state[0], self.learned_theta_value,
+                                         self.discrete_actions[
+                                             0])  # we take index zero since they are lists of numpy matrices
 
     def _make_additional_functions(self):
         # get trainable parameters
@@ -446,10 +482,16 @@ class GradPBO(object):
         self.T_grad_bellman_err = T.grad(self.T_bellman_err, params)
         # compile all functions
         # self.F_bellman_operator = theano.function([theta], self.bellman_model.outputs[0])
-        self.F_bellman_operator = theano.function([theta], self.bellman_model.model(theta))
-        self.F_q = theano.function([self.T_s, self.T_a, theta], self.q_model.model(self.T_s, self.T_a, theta))
+        self.F_bellman_operator = theano.function([theta],
+                                                  self.bellman_model.model(
+                                                      theta))
+        self.F_q = theano.function([self.T_s, self.T_a, theta],
+                                   self.q_model.model(self.T_s, self.T_a,
+                                                      theta))
         self.F_bellman_err = theano.function(
-            [self.T_s, self.T_a, self.T_s_next, self.T_r, theta, self.T_discrete_actions], self.T_bellman_err)
+            [self.T_s, self.T_a, self.T_s_next, self.T_r, self.T_absorbing,
+             theta, self.T_discrete_actions], self.T_bellman_err)
         self.F_grad_bellman_berr = theano.function(
-            [self.T_s, self.T_a, self.T_s_next, self.T_r, theta, self.T_discrete_actions],
+            [self.T_s, self.T_a, self.T_s_next, self.T_r, self.T_absorbing,
+             theta, self.T_discrete_actions],
             self.T_grad_bellman_err)

tests/pbo_grad_test.py
@@ -4,9 +4,6 @@ import numpy as np
 from scipy import optimize
 import theano
 import theano.tensor as T
-from itertools import chain, combinations
-
-# np.random.seed(14151)
 from sklearn.preprocessing import PolynomialFeatures
 
 
@@ -34,7 +31,9 @@ def bellmanop_grad(rho, theta):
     return np.kron(theta.T, np.eye(rho.shape[0], rho.shape[1]))
 
 
-def empirical_bop(s, a, r, snext, all_a, gamma, rho, theta, norm_value=2, incremental=False):
+def empirical_bop(s, a, r, snext, absorbing,
+                  all_a, gamma, rho, theta, norm_value=2,
+                  incremental=False):
     new_theta = bellmanop(rho, theta)
     if incremental:
         new_theta = theta + new_theta
@@ -45,7 +44,7 @@ def empirical_bop(s, a, r, snext, all_a, gamma, rho, theta, norm_value=2, increm
             qv = lqr_reg(snext[i], all_a[j], theta)
             if qv > bop[i]:
                 bop[i] = qv
-    v = qnop - r - gamma * bop
+    v = qnop - r - gamma * bop * (1. - absorbing)
     if norm_value == np.inf:
         err = np.max(np.abs(v))
     elif norm_value % 2 == 0:
@@ -55,18 +54,24 @@ def empirical_bop(s, a, r, snext, all_a, gamma, rho, theta, norm_value=2, increm
     return err, new_theta
 
 
-def multi_step_ebop(s, a, r, snext, all_a, gamma, rho, theta, norm_value=2, incremental=False, steps=1):
+def multi_step_ebop(s, a, r, snext, absorbing,
+                    all_a, gamma, rho, theta, norm_value=2,
+                    incremental=False, steps=1):
     tot_err = 0.0
     t = theta
     for k in range(steps):
-        err_k, t = empirical_bop(s, a, r, snext, all_a, gamma, rho, t, norm_value, incremental)
+        err_k, t = empirical_bop(s, a, r, snext, absorbing,
+                                 all_a, gamma, rho, t,
+                                 norm_value, incremental)
         tot_err += err_k
     return tot_err, t
 
+
 class LBPO(object):
     def __init__(self, init_rho):
-        self.rho = theano.shared(value=np.array(init_rho, dtype=theano.config.floatX),
-                                 borrow=True, name='rho')
+        self.rho = theano.shared(
+            value=np.array(init_rho, dtype=theano.config.floatX),
+            borrow=True, name='rho')
         self.theta = T.matrix()
         self.outputs = [T.dot(self.theta, self.rho)]
         self.inputs = [self.theta]
@@ -83,7 +88,8 @@ class LBPO(object):
 
 class LQRRegressor(object):
     def model(self, s, a, omega):
-        q = - omega[:, 0] ** 2 * s * a - 0.5 * omega[:, 1] * a * a - 0.4 * omega[:, 1] * s * s
+        q = - omega[:, 0] ** 2 * s * a \
+            - 0.5 * omega[:, 1] * a * a - 0.4 * omega[:, 1] * s * s
         return q.ravel()
 
 
@@ -93,8 +99,9 @@ class LQG_PBO(object):
         # define output for b
         combinations = PolynomialFeatures._combinations(2, 3, False, False)
         n_output_features_ = sum(1 for _ in combinations) + 1
-        self.A_b = theano.shared(value=np.ones((n_output_features_,), dtype=theano.config.floatX),
-                                 borrow=True, name='A_b')
+        self.A_b = theano.shared(
+            value=np.ones((n_output_features_,), dtype=theano.config.floatX),
+            borrow=True, name='A_b')
         self.b_b = theano.shared(value=1.,
                                  borrow=True, name='b_b')
 
@@ -108,8 +115,9 @@ class LQG_PBO(object):
         # define output for k
         combinations = PolynomialFeatures._combinations(2, 2, False, False)
         n_output_features_ = sum(1 for _ in combinations) + 1
-        self.rho_k = theano.shared(value=np.ones((n_output_features_,), dtype=theano.config.floatX),
-                                   borrow=True, name='rho_k')
+        self.rho_k = theano.shared(
+            value=np.ones((n_output_features_,), dtype=theano.config.floatX),
+            borrow=True, name='rho_k')
 
         combinations = PolynomialFeatures._combinations(2, 2, False, False)
         L = [(self.theta[:, 0] ** 0).reshape([-1, 1])]
@@ -144,7 +152,8 @@ s = np.array([1., 2., 3.]).reshape(-1, 1)
 a = np.array([0., 3., 4.]).reshape(-1, 1)
 nexts = s + 1
 r = np.array([-1., -5., 0.])
-discrete_actions = np.array([1, 2, 3]).reshape(-1, 1)  # discretization of the actions
+absorbing = np.array([0., 0., 0.])
+discrete_actions = np.array([1, 2, 3]).reshape(-1, 1)
 # to be used for maximum estimate
 
 # =================================================================
@@ -160,36 +169,22 @@ assert np.allclose(bellmanop(rho, theta), gpbo.F_bellman_operator(theta)), \
     '{}, {}'.format(bellmanop(rho, theta), gpbo.F_bellman_operator(theta))
 assert np.allclose(lqr_reg(s, a, theta), gpbo.F_q(s, a, theta))
 
-berr = gpbo.F_bellman_err(s, a, nexts, r, theta, discrete_actions)
-tv = multi_step_ebop(s, a, r, nexts, discrete_actions, gamma, rho, theta,
+berr = gpbo.F_bellman_err(s, a, nexts, r, absorbing, theta, discrete_actions)
+tv = multi_step_ebop(s, a, r, nexts, absorbing,
+                     discrete_actions, gamma, rho, theta,
                      norm_value=NORM_VAL, incremental=INCREMENTAL, steps=ST)[0]
 assert np.allclose(berr, tv), '{}, {}'.format(berr, tv)
 print(tv)
 
-berr_grad = gpbo.F_grad_bellman_berr(s, a, nexts, r, theta, discrete_actions)
+berr_grad = gpbo.F_grad_bellman_berr(s, a, nexts, r, absorbing,
+                                     theta, discrete_actions)
 eps = np.sqrt(np.finfo(float).eps)
-f = lambda x: multi_step_ebop(s, a, r, nexts, discrete_actions, gamma, x, theta,
-                              norm_value=NORM_VAL, incremental=INCREMENTAL, steps=ST)[0]
-approx_grad = optimize.approx_fprime(rho.ravel(), f, eps).reshape(berr_grad[0].shape)
+f = lambda x: multi_step_ebop(s, a, r, nexts, absorbing,
+                              discrete_actions, gamma, x, theta,
+                              norm_value=NORM_VAL, incremental=INCREMENTAL,
+                              steps=ST)[0]
+approx_grad = optimize.approx_fprime(rho.ravel(), f, eps).reshape(
+    berr_grad[0].shape)
 print("{}\n{}".format(berr_grad, approx_grad))
-assert np.allclose(berr_grad, approx_grad), '{}, {}'.format(berr_grad, approx_grad)
-
-# =================================================================
-# TODO
-# it is also possible to use keras model
-from keras.models import Sequential
-from keras.layers import Dense
-
-def _model_evaluation(self, theta):
-    inv = theta
-    for el in self.flattened_layers:
-        print(el)
-        inv = el(inv)
-    return inv
-
-
-Sequential._model_evaluation = _model_evaluation
-model = Sequential()
-model.add(Dense(4, input_dim=2, init='uniform', activation='relu'))
-model.add(Dense(2, init='uniform', activation='linear'))
-model.compile(loss='mse', optimizer='adam', metrics=['accuracy'])
+assert np.allclose(berr_grad, approx_grad), '{}, {}'.format(berr_grad,
+                                                            approx_grad)