minor fix on hessian

reward_space/lqg_run.py
@@ -5,151 +5,209 @@ import numpy as np
 from numpy import linalg as LA
 import ifqi.envs as envs
 from policy import GaussianPolicy1D
-from utils.utils import chebvalNd, MinMaxScaler, remove_projections, compute_feature_matrix, find_basis
+from utils.utils import chebvalNd, remove_projections, compute_feature_matrix, find_basis
 from utils.continuous_env_sample_estimator import ContinuousEnvSampleEstimator
 import utils.linalg2 as la2
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
-
-mdp = LQG1D()
-
-#MDP parameters
-discount_factor = mdp.gamma
-horizon = mdp.horizon
-max_action = mdp.max_action
-max_pos = mdp.max_pos
-state_dim, action_dim, reward_dim = envs.get_space_info(mdp)
-
-#Policy parameters
-K = mdp.computeOptimalK()
-sigma = 0.01
-
-policy = GaussianPolicy1D(K,sigma)
-
-#Collect samples
-n_episodes = 20
-dataset = evaluation.collect_episodes(mdp, policy, n_episodes)
-dataset = dataset[(np.arange(500,1000) + np.arange(0,20000,1000)[:,np.newaxis]).ravel()]
-
-estimator = ContinuousEnvSampleEstimator(dataset, mdp.gamma)
-
-states_actions = dataset[:,:2]
-states = dataset[:,0]
-actions = dataset[:,1]
-discounts = dataset[:,4]
-rewards = dataset[:,2]
-
-print('Dataset (sigma %f) has %d samples' % (sigma, dataset.shape[0]))
-
-#Scale data
-bounds = [[-max_pos, max_pos], [-max_action ,max_action]]
-scaler = MinMaxScaler(ndim=2, input_ranges=bounds)
-scaled_states_actions = scaler.scale(states_actions)
-scaled_states = scaled_states_actions[:,0]
-scaled_actions = scaled_states_actions[:,1]
-
-#Compute feature matrix    
-
-complement = [lambda x : policy.gradient_log_pdf(x[0],x[1])]
-max_degree = 5
-degrees = [[ds,da] for ds in range(max_degree+1) for da in range(max_degree+1)]
-cheb_basis = map(lambda d: lambda x: chebvalNd(x, d), degrees)
-n_samples = dataset.shape[0]
-n_features = len(cheb_basis)
-n_complement = 1
-
-X = compute_feature_matrix(n_samples, n_features, scaled_states, scaled_actions, cheb_basis)
-C = compute_feature_matrix(n_samples, n_complement, states, actions, complement)
-
-W = estimator.d_sasa_mu
-X_ort = remove_projections(X, C, W)
-#X_ort = remove_projections(X, C, np.diag(discounts)) 
-#Non mi interessa che sia ortonormale!!!
-#X_ort_ort = find_basis(X_ort, np.diag(discounts))
-#print('Rank of feature matrix X %s/%s' % (X_ort_ort.shape[1], X.shape[1]))
-
-rewards_hat, w, rmse, _ = la2.lsq(X_ort, rewards)
-error = np.abs(rewards - rewards_hat)
-mae = np.mean(error)
-error_rel = np.abs((rewards - rewards_hat)/rewards)
-mare = np.mean(error_rel)
+from sklearn.preprocessing import MinMaxScaler
 
 
-grad_J_true = 1.0/n_episodes * LA.multi_dot([C.T, W, rewards])
-grad_J_hat = 1.0/n_episodes * LA.multi_dot([C.T, W, rewards])
-J_hat = 1.0/n_episodes * np.sum(rewards * discounts)
-print('Results of LS rmse = %s mae = %s mare = %s' % (rmse, mae, mare))
-print('True policy gradient %s' % grad_J_true)
-print('Estimated policy gradient %s' % grad_J_hat)
-print('Estimated expected return %s' % J_hat)
+def estimate_hessian(dataset, n_episodes, policy_gradient, policy_hessian, reward_features):
 
-'''
+    n_samples = dataset.shape[0]
+    n_features = reward_features.shape[1]
+    n_params = policy_hessian.shape[1]
 
-#---------------------------Q-function evaluation-----------------------------
-Q_true = np.array(map(lambda s,a: mdp.computeQFunction(s, a, K, np.power(sigma,2)), states, actions))
-Q_hat, w, rmse = estimate_Q(X_ort_ort, Q_true)
-error = np.abs(Q_true - Q_hat)
-mae = np.mean(error)
-error_rel = np.abs((Q_true - Q_hat)/Q_true)
-mare = np.mean(error_rel)
+    episode_reward_features = np.zeros((n_episodes, n_features))
+    episode_hessian = np.zeros((n_episodes, n_params, n_params))
 
-W = np.diag(discounts)
+    #baseline = estimate_hessian_baseline(dataset, n_episodes, policy_gradient, policy_hessian, reward_features, state_space, action_space)
 
-grad_J_true = 1.0/n_episodes * LA.multi_dot([C.T, W, Q_true])
-grad_J_hat = 1.0/n_episodes * LA.multi_dot([C.T, W, Q_hat])
-J_hat = 1.0/n_episodes * np.sum(rewards * discounts)
-print('Results of LS rmse = %s mae = %s mare = %s' % (rmse, mae, mare))
-print('True policy gradient %s' % grad_J_true)
-print('Estimated policy gradient %s' % grad_J_hat)
-print('Estimated expected return %s' % J_hat)
+    i = 0
+    episode = 0
+    while i < n_samples:
+        index = i
 
-#-------------------------Plot------------------------------------------------
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-ax.scatter(states, actions, Q_true, c='r', marker='o')
-ax.scatter(states, actions, Q_hat, c='b', marker='^')
-ax.set_xlabel('s')
-ax.set_ylabel('a')
-ax.set_zlabel('Q(s,a)')
+        d = dataset[i, 4]
 
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-ax.scatter(states, actions, error, c='g', marker='*')
-ax.set_xlabel('s')
-ax.set_ylabel('a')
-ax.set_zlabel('error(s,a)')
+        episode_reward_features[episode, :] += d * reward_features[index, :].squeeze()
+        episode_hessian[episode, :, :] += np.outer(
+            policy_gradient[index, :].squeeze(), \
+            policy_gradient[index, :].squeeze()) + \
+            policy_hessian[index, :, :].squeeze()
 
-fig = plt.figure()
-ax = fig.add_subplot(111)
-ax.scatter(states, error, c='g', marker='*')
-ax.set_xlabel('s')
-ax.set_ylabel('|Q_true(s,*) - Q_hat(s,*)|')
+        if dataset[i, -1] == 1:
+            episode += 1
 
-fig = plt.figure()
-ax = fig.add_subplot(111)
-ax.scatter(states, error_rel, c='g', marker='*')
-ax.set_xlabel('s')
-ax.set_ylabel('|Q_true(s,*) - Q_hat(s,*)|/|Q_true(s,*)|')
+        i += 1
 
-fig = plt.figure()
-ax = fig.add_subplot(111)
-ax.scatter(states[:n_episodes], Q_true[:n_episodes], c='r', marker='o')
-ax.scatter(states[:n_episodes], Q_hat[:n_episodes], c='b', marker='^')
-ax.set_xlabel('s')
-ax.set_ylabel('Q(s,*)')
+    episode_reward_features_baseline = episode_reward_features #- baseline
 
-fig = plt.figure()
-ax = fig.add_subplot(111)
-ax.scatter(actions[:n_episodes], Q_true[:n_episodes], c='r', marker='o')
-ax.scatter(actions[:n_episodes], Q_hat[:n_episodes], c='b', marker='^')
-ax.set_xlabel('a')
-ax.set_ylabel('Q(*,a)')
+    return_hessians = 1. / n_episodes * np.tensordot(episode_reward_features_baseline.T,
+                                                     episode_hessian, axes=1)
 
-fig = plt.figure()
-ax = fig.add_subplot(111)
-ax.scatter(states[:n_episodes], error_rel[:n_episodes], c='g', marker='*')
-ax.set_xlabel('s')
-ax.set_ylabel('|Q_true(s,*) - Q_hat(s,*)|/|Q_true(s,*)|')
+    return return_hessians
 
-plt.show()
-'''
+
+if __name__ == '__main__':
+    mdp = LQG1D()
+    #Policy parameters
+    K = mdp.computeOptimalK()
+    sigma = 0.01
+
+    policy = GaussianPolicy1D(K,sigma)
+
+    #Collect samples
+    n_episodes = 20
+    dataset = evaluation.collect_episodes(mdp, policy, n_episodes)
+    #dataset = dataset[(np.arange(500,1000) + np.arange(0,20000,1000)[:,np.newaxis]).ravel()]
+
+    estimator = ContinuousEnvSampleEstimator(dataset, mdp.gamma)
+    d_sa_mu_hat = estimator.get_d_sa_mu()
+    D_hat = np.diag(d_sa_mu_hat)
+
+    states = dataset[:, 0]
+    actions = dataset[:, 1]
+    rewards = dataset[:, 2]
+    G = map(lambda s,a: policy.gradient_log(s,a), states, actions)
+    H = map(lambda s,a: policy.hessian_log(s,a), states, actions)
+    G = np.array(G)
+    H = np.array(H)
+
+
+    print('-' * 100)
+    print('Computing Q-function approx space...')
+    X = np.dot(G.T, D_hat)
+    phi = la2.nullspace(X)
+    psi = phi
+
+    scaler = MinMaxScaler(copy=False)
+    psi = scaler.fit_transform(psi)
+    r = scaler.fit_transform(rewards[:, np.newaxis])
+
+    h = estimate_hessian(dataset, n_episodes, G, H, psi)
+    plt.scatter(np.arange(len(h)), h)
+
+    h = estimate_hessian(dataset, n_episodes, G, H, r)
+    plt.scatter(np.arange(len(h)), h, color='r')
+
+    '''
+    states_actions = dataset[:,:2]
+    discounts = dataset[:,4]
+    rewards = dataset[:,2]
+
+    print('Dataset (sigma %f) has %d samples' % (sigma, dataset.shape[0]))
+
+    #Scale data
+    bounds = [[-max_pos, max_pos], [-max_action ,max_action]]
+    scaler = MinMaxScaler(ndim=2, input_ranges=bounds)
+    scaled_states_actions = scaler.scale(states_actions)
+    scaled_states = scaled_states_actions[:,0]
+    scaled_actions = scaled_states_actions[:,1]
+
+    #Compute feature matrix
+
+    complement = [lambda x : policy.gradient_log_pdf(x[0],x[1])]
+    max_degree = 5
+    degrees = [[ds,da] for ds in range(max_degree+1) for da in range(max_degree+1)]
+    cheb_basis = map(lambda d: lambda x: chebvalNd(x, d), degrees)
+    n_samples = dataset.shape[0]
+    n_features = len(cheb_basis)
+    n_complement = 1
+
+    X = compute_feature_matrix(n_samples, n_features, scaled_states, scaled_actions, cheb_basis)
+    C = compute_feature_matrix(n_samples, n_complement, states, actions, complement)
+
+    W = estimator.d_sasa_mu
+    X_ort = remove_projections(X, C, W)
+    #X_ort = remove_projections(X, C, np.diag(discounts))
+    #Non mi interessa che sia ortonormale!!!
+    #X_ort_ort = find_basis(X_ort, np.diag(discounts))
+    #print('Rank of feature matrix X %s/%s' % (X_ort_ort.shape[1], X.shape[1]))
+
+    rewards_hat, w, rmse, _ = la2.lsq(X_ort, rewards)
+    error = np.abs(rewards - rewards_hat)
+    mae = np.mean(error)
+    error_rel = np.abs((rewards - rewards_hat)/rewards)
+    mare = np.mean(error_rel)
+
+
+    grad_J_true = 1.0/n_episodes * LA.multi_dot([C.T, W, rewards])
+    grad_J_hat = 1.0/n_episodes * LA.multi_dot([C.T, W, rewards])
+    J_hat = 1.0/n_episodes * np.sum(rewards * discounts)
+    print('Results of LS rmse = %s mae = %s mare = %s' % (rmse, mae, mare))
+    print('True policy gradient %s' % grad_J_true)
+    print('Estimated policy gradient %s' % grad_J_hat)
+    print('Estimated expected return %s' % J_hat)
+
+
+
+    #---------------------------Q-function evaluation-----------------------------
+    Q_true = np.array(map(lambda s,a: mdp.computeQFunction(s, a, K, np.power(sigma,2)), states, actions))
+    Q_hat, w, rmse = estimate_Q(X_ort_ort, Q_true)
+    error = np.abs(Q_true - Q_hat)
+    mae = np.mean(error)
+    error_rel = np.abs((Q_true - Q_hat)/Q_true)
+    mare = np.mean(error_rel)
+
+    W = np.diag(discounts)
+
+    grad_J_true = 1.0/n_episodes * LA.multi_dot([C.T, W, Q_true])
+    grad_J_hat = 1.0/n_episodes * LA.multi_dot([C.T, W, Q_hat])
+    J_hat = 1.0/n_episodes * np.sum(rewards * discounts)
+    print('Results of LS rmse = %s mae = %s mare = %s' % (rmse, mae, mare))
+    print('True policy gradient %s' % grad_J_true)
+    print('Estimated policy gradient %s' % grad_J_hat)
+    print('Estimated expected return %s' % J_hat)
+
+    #-------------------------Plot------------------------------------------------
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.scatter(states, actions, Q_true, c='r', marker='o')
+    ax.scatter(states, actions, Q_hat, c='b', marker='^')
+    ax.set_xlabel('s')
+    ax.set_ylabel('a')
+    ax.set_zlabel('Q(s,a)')
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.scatter(states, actions, error, c='g', marker='*')
+    ax.set_xlabel('s')
+    ax.set_ylabel('a')
+    ax.set_zlabel('error(s,a)')
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.scatter(states, error, c='g', marker='*')
+    ax.set_xlabel('s')
+    ax.set_ylabel('|Q_true(s,*) - Q_hat(s,*)|')
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.scatter(states, error_rel, c='g', marker='*')
+    ax.set_xlabel('s')
+    ax.set_ylabel('|Q_true(s,*) - Q_hat(s,*)|/|Q_true(s,*)|')
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.scatter(states[:n_episodes], Q_true[:n_episodes], c='r', marker='o')
+    ax.scatter(states[:n_episodes], Q_hat[:n_episodes], c='b', marker='^')
+    ax.set_xlabel('s')
+    ax.set_ylabel('Q(s,*)')
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.scatter(actions[:n_episodes], Q_true[:n_episodes], c='r', marker='o')
+    ax.scatter(actions[:n_episodes], Q_hat[:n_episodes], c='b', marker='^')
+    ax.set_xlabel('a')
+    ax.set_ylabel('Q(*,a)')
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.scatter(states[:n_episodes], error_rel[:n_episodes], c='g', marker='*')
+    ax.set_xlabel('s')
+    ax.set_ylabel('|Q_true(s,*) - Q_hat(s,*)|/|Q_true(s,*)|')
+
+    plt.show()
+    '''

reward_space/policy.py
@@ -97,8 +97,11 @@ class GaussianPolicy1D(SimplePolicy):
     def seed(self, seed=None):
         self.np_random, seed = seeding.np_random(seed)
         
-    def gradient_log_pdf(self, state, action):
+    def gradient_log(self, state, action):
         return np.array(np.array(state/np.power(self.sigma,2)*(action - np.dot(self.K,state))).ravel(), ndmin=1)
+
+    def hessian_log(self, state, action):
+        return np.array(- state ** 2 / self.sigma ** 2, ndmin=2)
         
         
 class GaussianPolicy(SimplePolicy):
@@ -625,21 +628,45 @@ class BoltzmannPolicy(Policy):
         self.pi2[row_index, col_index] = self.pi
 
     def _build_grad_hess(self):
-        self.grad_log = np.zeros((self.n_states * self.n_actions, self.n_parameters * self.n_actions))
-        self.hess_log = np.zeros((self.n_states * self.n_actions, self.n_parameters * self.n_actions, self.n_parameters * self.n_actions))
+        self.grad_log = np.zeros((self.n_states * self.n_actions,
+                                  self.n_parameters * self.n_actions))
+        self.hess_log = np.zeros((self.n_states * self.n_actions,
+                                  self.n_parameters * self.n_actions,
+                                  self.n_parameters * self.n_actions))
+        #Compute the gradient for all (s,a) pairs
         for state in range(self.n_states):
-            row_index = state * self.n_actions + np.arange(self.n_actions)
-            expon = np.exp(np.dot(self.state_action_features[row_index], self.state_action_parameters))
-            num_sum = np.dot(self.state_action_features[row_index].T, expon).ravel()
-            den_sum = np.sum(expon)
 
-            hess_num_sum = la.multi_dot([self.state_action_features[row_index].T, np.diag(expon.ravel()),\
-                                         self.state_action_features[row_index]])
-            self.hess_log[row_index] = - (hess_num_sum * den_sum - np.outer(num_sum, num_sum)) / den_sum ** 2
+            num = den = 0
             for action in range(self.n_actions):
                 index = state * self.n_actions + action
-                phi = self.state_action_features[index]
-                self.grad_log[index] = phi - num_sum / den_sum
+                exponential = np.exp(np.dot(self.state_action_features[index],\
+                                            self.state_action_parameters))
+                num += self.state_action_features[index] * exponential
+                den += exponential
+
+            for action in range(self.n_actions):
+                index = state * self.n_actions + action
+                self.grad_log[index] = self.state_action_features[index] - num / den
+
+        #Compute the hessian for all (s,a) pairs
+        for state in range(self.n_states):
+
+            num1 = num2 = den1 = 0
+            for action in range(self.n_actions):
+                index = state * self.n_actions + action
+                exponential = np.exp(np.dot(self.state_action_features[index], \
+                                            self.state_action_parameters))
+                num1 += np.outer(self.state_action_features[index],\
+                                 self.state_action_features[index]) * exponential
+                num2 += self.state_action_features[index] * exponential
+                den1 += exponential
+
+            num = num1 * den1 - np.outer(num2, num2)
+            den = den1 ** 2
+
+            for action in range(self.n_actions):
+                index = state * self.n_actions + action
+                self.hess_log[index] = - num / den
 
     def pdf(self, state, action):
         num = np.exp(np.dot(self.features[state], self.parameters[action].T))

reward_space/taxi_run.py
@@ -82,14 +82,13 @@ def estimate_hessian(dataset, n_episodes, policy_gradient, policy_hessian, rewar
 
     n_samples = dataset.shape[0]
     n_features = reward_features.shape[1]
-    n_states_actions = policy_hessian.shape[0]
     n_params = policy_hessian.shape[1]
     n_states, n_actions = len(state_space), len(action_space)
 
     episode_reward_features = np.zeros((n_episodes, n_features))
     episode_hessian = np.zeros((n_episodes, n_params, n_params))
 
-    baseline = estimate_baseline(dataset, n_episodes, policy_gradient, policy_hessian, reward_features, state_space, action_space)
+    baseline = estimate_hessian_baseline(dataset, n_episodes, policy_gradient, policy_hessian, reward_features, state_space, action_space)
 
     i = 0
     episode = 0
@@ -118,11 +117,79 @@ def estimate_hessian(dataset, n_episodes, policy_gradient, policy_hessian, rewar
 
     return return_hessians
 
+def estimate_gradient(dataset, n_episodes, policy_gradient, reward_features, state_space, action_space):
 
-def estimate_baseline(dataset, n_episodes, policy_gradient, policy_hessian, reward_features, state_space, action_space):
     n_samples = dataset.shape[0]
     n_features = reward_features.shape[1]
-    n_states_actions = policy_hessian.shape[0]
+    n_params = policy_gradient.shape[1]
+    n_states, n_actions = len(state_space), len(action_space)
+
+    episode_reward_features = np.zeros((n_episodes, n_features))
+    episode_gradient = np.zeros((n_episodes, n_params))
+
+    baseline = estimate_gradient_baseline(dataset, n_episodes, policy_gradient, reward_features, state_space, action_space)
+
+    i = 0
+    episode = 0
+    while i < n_samples:
+        s = np.argwhere(state_space == dataset[i, 0])
+        a = np.argwhere(action_space == dataset[i, 1])
+        index = s * n_actions + a
+
+        d = dataset[i, 4]
+
+        episode_reward_features[episode, :] += d * reward_features[index, :].squeeze()
+        episode_gradient[episode, :] +=  policy_gradient[index, :].squeeze()
+
+        if dataset[i, -1] == 1:
+            episode += 1
+
+        i += 1
+
+    episode_reward_features_baseline = episode_reward_features - baseline
+
+    return_gradient = 1. / n_episodes * np.dot(episode_reward_features_baseline.T,
+                                                     episode_gradient)
+
+    return return_gradient
+
+def estimate_gradient_baseline(dataset, n_episodes, policy_gradient, reward_features, state_space, action_space):
+    n_samples = dataset.shape[0]
+    n_features = reward_features.shape[1]
+    n_params = policy_gradient.shape[1]
+    n_states, n_actions = len(state_space), len(action_space)
+
+    episode_reward_features = np.zeros((n_episodes, n_features))
+    episode_gradient = np.zeros((n_episodes, n_params))
+    numerator = denominator = 0.
+
+    i = 0
+    episode = 0
+    while i < n_samples:
+        s = np.argwhere(state_space == dataset[i, 0])
+        a = np.argwhere(action_space == dataset[i, 1])
+        index = s * n_actions + a
+
+        d = dataset[i, 4]
+
+        episode_reward_features[episode, :] += d * reward_features[index, :].squeeze()
+        episode_gradient[episode, :] += policy_gradient[index, :].squeeze()
+
+        if dataset[i, -1] == 1:
+            vectorized_gradient = episode_gradient[episode, :].ravel()
+            numerator += episode_reward_features[episode, :] * la.norm(vectorized_gradient) ** 2
+            denominator += la.norm(vectorized_gradient) ** 2
+            episode += 1
+
+        i += 1
+
+    baseline = numerator / denominator
+
+    return baseline
+
+def estimate_hessian_baseline(dataset, n_episodes, policy_gradient, policy_hessian, reward_features, state_space, action_space):
+    n_samples = dataset.shape[0]
+    n_features = reward_features.shape[1]
     n_params = policy_hessian.shape[1]
     n_states, n_actions = len(state_space), len(action_space)
 
@@ -167,7 +234,7 @@ def trace_minimization(hessians, features, threshold):
 
     objective = cvxpy.Minimize(cvxpy.trace(final_hessian))
     constraints = [final_hessian + threshold * np.eye(n_parameters) << 0,
-                   cvxpy.norm(w) <= 1]
+                   cvxpy.sum_entries(w) == 1]
     problem = cvxpy.Problem(objective, constraints)
 
     result = problem.solve(verbose=True)
@@ -189,12 +256,6 @@ def maximum_eigenvalue_minimizarion(hessians, features, threshold):
 
     result = problem.solve(verbose=True)
     return w.value, final_hessian.value, result
-
-
-#def heuristic_choice(hessians, features, threshold):
-
-
-
 
 def maximum_entropy(dataset, n_episodes, policy_gradient, reward_features, state_space, action_space):
 
@@ -474,11 +535,14 @@ if __name__ == '__main__':
 
     if plot_gradient:
         fig, ax = plt.subplots()
-        ax.set_xlabel('Singular value')
-        ax.set_ylabel('Index')
+        ax.set_xlabel('Index')
+        ax.set_ylabel('Singular value')
         fig.suptitle('Gradient singular values')
-        s = la.svd(G, compute_uv=False)
-        plt.plot(np.arange(len(s)), s, marker='o', label='Gradient')
+        s = la.svd(G.T, compute_uv=False)
+        D_idx = D.copy()
+        D_idx[D_idx > 0] = 1
+        s = la.svd(np.dot(G.T, D), compute_uv=False)
+        plt.plot(np.arange(len(s)), s, marker='o', label='Gradient * d(s,a)')
     '''
     #---------------------------------------------------------------------------
     #Q-function and A-function estimation with PVFs
@@ -616,15 +680,17 @@ if __name__ == '__main__':
                 ax2.legend(loc='upper right')
 
     '''
+
     print('-' * 100)
 
     print('Computing Q-function approx space...')
     X = np.dot(G.T, D_hat)
     phi = la2.nullspace(X)
 
-    print('Computing reward function approx space...')
-    Y = np.dot(np.eye(mdp_wrap.nA * mdp_wrap.nS) - pi_tilde, phi)
-    psi = la2.range(Y)
+    psi = phi
+    #print('Computing reward function approx space...')
+    #Y = np.dot(np.eye(mdp_wrap.nA * mdp_wrap.nS) - pi_tilde, phi)
+    #psi = la2.range(Y)
 
     scaler = MinMaxScaler(copy=False)
     psi = scaler.fit_transform(psi)
@@ -653,7 +719,6 @@ if __name__ == '__main__':
     min_trace_idx = trace_hat.argmin()
     max_trace_idx = trace_hat.argmax()
 
-
     print('Computing max eigenvalue...')
     eigval_true, _ = la.eigh(hessian_true)
     eigmax_true = eigval_true[-1]
@@ -673,13 +738,16 @@ if __name__ == '__main__':
                    label='Reward function')
         ax.plot(np.arange(len(eigval_true)), eigval_true_a, color='g', marker='d',
                    label='Advantage function')
-        '''
         ax.plot(np.arange(len(eigval_true)), eigval_hat[min_trace_idx], color='b', marker='o',
                    label='Feature with smallest trace')
         ax.plot(np.arange(len(eigval_true)), eigval_hat[max_trace_idx], color='m', marker='o',
                    label='Feature with largest trace')
         '''
+        r = np.hstack([np.arange(len(eigval_true))[:, np.newaxis] + 1, eigval_true[:, np.newaxis], eigval_true_a[:, np.newaxis], eigval_hat[min_trace_idx][:, np.newaxis], eigval_hat[max_trace_idx][:, np.newaxis]])
+        np.savetxt('eigen.csv', r, delimiter=',', header='x,reward function,advantage function,Feature with smallest trace,Feature with largest trace')
+
         ax.legend(loc='upper right')
+        '''
     '''
     print('Trace minimization...')
     print(time.strftime('%x %X %z'))
@@ -698,7 +766,7 @@ if __name__ == '__main__':
     r_hat_eigval /= la.norm(r_hat_eigval)
     trace_eigval = np.trace(hessian_eigval)
     eigmax_eigval = la.eigh(hessian_eigval)[0][-1]
-    '''
+
     if plot_hessians:
         fig, ax = plt.subplots()
         ax.set_xlabel('trace')
@@ -711,7 +779,7 @@ if __name__ == '__main__':
         #ax.scatter(trace_trace, eigmax_trace, color='g', marker='d', s=100)
         #ax.scatter(trace_eigval, eigmax_eigval, color='y', marker='d', s=100)
 
-        '''
+
         fig, ax = plt.subplots()
         ax.set_xlabel('feature')
         ax.set_ylabel('eigval-trace')
@@ -726,7 +794,7 @@ if __name__ == '__main__':
         ax.plot(np.arange(len(trace_hat)), [trace_true]*len(trace_hat), label='True Trace')
         ax.plot(np.arange(len(eigmax_hat)),[eigmax_true]*len(trace_hat), label='True MaxEigval')
         ax.legend(loc='upper right')
-        '''
+
     plt.show()
 
 #guardare segno negativo
@@ -801,9 +869,4 @@ def less_trivial_heuristic(hessians, max_iter=100, tolerance=1e-10):
 
     print(ite)
     return weights
-
-
-
-
-
-
+'''